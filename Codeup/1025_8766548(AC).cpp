8ea0cd4e225322f398634c2dc2fc81c570633ed1
UnluckyJung <ybook2012@gmail.com>
2019-05-09 05:16:10 +0900

    12345 / 10000 = 1 1 * 10000 = 10000을 이용함.
    12345 % 10000 = 2345

---
+++ Codeup/1025_8766548(AC).cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	
+	int num;
+	int div = 10000;
+	scanf("%d", &num);
+
+	if (10000 <= num && num <= 99999) {
+		while (num > 0) {
+
+			printf("[%d]\n", num / div * div);
+			num = num % div;
+			div = div / 10;
+		}
+	}
+
+	return 0;
+}
