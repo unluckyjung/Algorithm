4948a6b54f0ea0c516e118cd236b685cdd605395
UnluckyJung <ybook2012@gmail.com>
2019-05-14 00:31:00 +0900

    1044 , 1045 .2lf

---
+++ Codeup/1044_8827773(AC).cpp
@@ -0,0 +1,11 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+
+	long long num;
+
+	scanf("%lld", &num);
+	printf("%lld", num + 1);
+
+	return 0;
+}
---
+++ Codeup/1045_8827783(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+
+	int n1, n2;
+
+	scanf("%d%d", &n1, &n2);
+
+	printf("%d\n", n1 + n2);
+	printf("%d\n", n1 - n2);
+	printf("%d\n", n1 * n2);
+	printf("%d\n", n1 / n2);
+	printf("%d\n", n1 % n2);
+	printf("%.2lf", (double)n1 / (double)n2);
+
+	return 0;
+}
