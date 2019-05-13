3d858fb19988d4ba3bfd13e88baa3d705e80877f
UnluckyJung <ybook2012@gmail.com>
2019-05-14 00:32:48 +0900

    1046

---
+++ Codeup/1046_8827787(AC).cpp
@@ -0,0 +1,13 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+
+	int n1, n2, n3;
+
+	scanf("%d%d%d", &n1, &n2, &n3);
+
+	printf("%d\n", n1 + n2 +n3);
+	printf("%.1lf", double(n1 + n2 + n3)/3);
+
+	return 0;
+}
