1bdd8b021a8ff690dfc53efd7f2ecccbc2b580ce
UnluckyJung <ybook2012@gmail.com>
2019-05-17 02:43:34 +0900

    3항 연산자 순서에 대해서 조금 더 고민할 수 있게 해주는 문제.

---
+++ Codeup/1064_8874572(AC).cpp
@@ -0,0 +1,10 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	int n1, n2, n3;
+	scanf("%d %d %d", &n1, &n2, &n3);
+	printf("%d", (n1 < n2 ? n1 : n2) < n3 ? (n1 < n2 ? n1 : n2) : n3);
+	//n1이 n2보다 작다면 n1이 선정되어짐. 이것이 n3 보다 작다면, 
+	//n1 선정되고, 아니면 n3이된다.
+	return 0;
+}
