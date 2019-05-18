bc521b12fb520158574a214a4676d89f02858366
UnluckyJung <ybook2012@gmail.com>
2019-05-18 17:03:01 +0900

    even or odd

---
+++ Codeup/1066_8895777(AC).cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	int num[3] = { 0, };
+
+	scanf("%d%d%d", &num[0], &num[1], &num[2]);
+
+	for (int i = 0; i < 3; i++) {
+		if (num[i] % 2 == 0)
+			printf("even\n");
+		else
+			printf("odd\n");
+	}
+	return 0;
+}
