c7da233674d265e99f20dd10b53e1144314a08ad
UnluckyJung <ybook2012@gmail.com>
2019-05-18 17:06:10 +0900

    + - even odd

---
+++ Codeup/1067_8895808(AC).cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	int num;
+
+	scanf("%d", &num);
+
+	if (num > 0) {
+		printf("plus\n");
+		if (num % 2 == 0) {
+			printf("even");
+		}
+		else
+			printf("odd");
+	}
+	else {
+		printf("minus\n");
+		if (num % 2 == 0) {
+			printf("even");
+		}
+		else
+			printf("odd");
+	}
+	return 0;
+}
