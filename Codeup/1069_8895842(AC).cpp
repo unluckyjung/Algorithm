7b186045af0f7c26977edb4cee6a3cb026ab6f6a
UnluckyJung <ybook2012@gmail.com>
2019-05-18 17:09:18 +0900

    grede ABCD

---
+++ Codeup/1069_8895842(AC).cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	char grade;
+	scanf("%c", &grade);
+
+	switch (grade) {
+	case 'A' :
+		printf("best!!!");
+		break;
+	case 'B':
+		printf("good!!");
+		break;
+	case 'C':
+		printf("run!");
+		break;
+	case 'D':
+		printf("slowly~");
+		break;
+	default:
+		printf("what?");
+		break;
+	}
+	return 0;
+}
