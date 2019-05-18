e5e38d1f5171604bb7f2cc87322951234696fc15
UnluckyJung <ybook2012@gmail.com>
2019-05-18 17:36:27 +0900

    season

---
+++ Codeup/1070_8896101(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	int grade;
+	scanf("%d", &grade);
+	
+	if (grade == 12 || grade == 1 || grade == 2)
+		printf("winter\n");
+	else if (grade == 3 || grade == 4 || grade == 5)
+		printf("spring\n");
+	else if (grade == 6 || grade == 7 || grade == 8)
+		printf("summer\n");
+	else
+		printf("fall\n");
+	return 0;
+}
