2d74734c698f757036e972001799b9d0641691e7
UnluckyJung <ybook2012@gmail.com>
2019-05-18 17:19:57 +0900

    ABCD 90 80 70

---
+++ Codeup/1068_8895953(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	int grade;
+	scanf("%d", &grade);
+	
+	if (grade >= 90)
+		printf("A\n");
+	else if (grade >= 70)
+		printf("B\n");
+	else if (grade >= 40)
+		printf("C\n");
+	else
+		printf("D\n");
+	return 0;
+}
