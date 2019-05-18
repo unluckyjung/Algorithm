91a9245ff794641e4369188e94efc44a208a87fb
UnluckyJung <ybook2012@gmail.com>
2019-05-18 17:53:40 +0900

    goto skill.. for나 while문이 사용불가능하다.

---
+++ Codeup/1071_8896258(AC).cpp
@@ -0,0 +1,15 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+	int n;
+
+reload:
+	scanf("%d", &n);	//3 5가 입력시
+
+	printf("%d\n", n);	//3을 받아서 3\n이 출력되고 다시 5를받아서 5\n이 출력된다.
+	if (n == 0)
+		return 0;
+	goto reload;
+
+	return 0;
+}
