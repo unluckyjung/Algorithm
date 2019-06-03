fd5e2d0ca03ae323a33b93942cbb0e9f836337a8
UnluckyJung <ybook2012@gmail.com>
2019-06-03 23:38:15 +0900

    1076

---
+++ Codeup/1076_9214549(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	char al;
+	char al2 = 'a';
+
+	scanf("%c", &al);
+
+	while (al >= 97) {
+		printf("%c ", al2++);
+		al--;
+	}
+	return 0;
+}
+
