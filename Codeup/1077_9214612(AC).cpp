4f89b872c5d6b2c7f3eaaef545ffafe4b6864a01
UnluckyJung <ybook2012@gmail.com>
2019-06-03 23:49:17 +0900

    1077

---
+++ Codeup/1077_9214612(AC).cpp
@@ -0,0 +1,16 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int num;
+	int count = 0;
+	scanf("%d", &num);
+	while (num >= 0) {
+		printf("%d\n", count++);
+		num--;
+	}
+
+	return 0;
+}
+
