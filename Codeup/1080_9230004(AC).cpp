d91810deccf0e6912282e5d4124c2807fcedb096
UnluckyJung <ybook2012@gmail.com>
2019-06-05 00:13:07 +0900

    1080

---
+++ Codeup/1080_9230004(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num1;
+	int num2 = 0, sum = 0;
+
+	scanf("%d", &num1);
+	while (sum < num1) {
+		num2++;
+		sum += num2;
+	}
+	printf("%d\n", num2);
+
+	return 0;
+}
+
