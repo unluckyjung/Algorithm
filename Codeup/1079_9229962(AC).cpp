421253c9c39bbfd7faa90c65c9244eb1bf949abe
UnluckyJung <ybook2012@gmail.com>
2019-06-05 00:07:41 +0900

    %c 문자 하나를 이용하면 두칸씩 enter가 눌림. 확인해 봐야할듯.

---
+++ Codeup/1079_9229962(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	char alpha[5];
+
+	while (1) {
+		scanf("%s", &alpha);
+		if (!(strcmp(alpha, "q"))) {
+			printf("%s\n", alpha);
+			break;
+		}
+		printf("%s\n", alpha);
+	}
+	return 0;
+}
+
