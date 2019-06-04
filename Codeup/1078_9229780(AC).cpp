abefe75ba766126addf7ea18050c46cd672961dc
UnluckyJung <ybook2012@gmail.com>
2019-06-04 23:46:38 +0900

    Even sum

---
+++ Codeup/1078_9229780(AC).cpp
@@ -0,0 +1,18 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	int num;
+	int sum = 0;
+	scanf("%d", &num);
+
+	while(num) {
+		if (!(num&1))
+			sum += num;
+		num--;
+	}
+	printf("%d\n", sum);
+	return 0;
+}
+
