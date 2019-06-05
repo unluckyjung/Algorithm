4a7260fbed7fb9b729030b3ddbdd5e0a86c468cb
UnluckyJung <ybook2012@gmail.com>
2019-06-06 00:56:45 +0900

    1081

---
+++ Codeup/1081_9252286(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n1, n2;
+	scanf("%d %d", &n1, &n2);
+	
+
+	for (int i = 1; i <= n1; i++) {
+		for (int j = 1; j <= n2; j++) {
+			printf("%d %d\n", i, j);
+		}
+	}
+
+	return 0;
+}
+
