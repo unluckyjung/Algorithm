31a2b2e88cccafc40517c7ff551107c5d8cced1b
UnluckyJung <ybook2012@gmail.com>
2019-06-06 01:00:11 +0900

    Hex gugu

---
+++ Codeup/1082_9252311(AC).cpp
@@ -0,0 +1,13 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num;
+	scanf("%x", &num);
+	for (int i = 1; i < 16; i++) {
+		printf("%X*%X=%X\n", num, i,num*i);
+	}
+
+	return 0;
+}
+
