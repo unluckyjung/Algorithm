fbf24cc64f471b1a909fcbb37f0b0c5434688ae6
UnluckyJung <ybook2012@gmail.com>
2019-06-07 22:44:43 +0900

    find min

---
+++ Codeup/1095_9272397(AC).cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num;
+	int num2;
+	int min;
+
+	scanf("%d", &num);
+	min = 100;
+	for (int i = 1; i <= num; i++) {
+		scanf("%d", &num2);
+		getchar();	//엔터 버퍼를 지워준다.
+		if (min > num2)
+			min = num2;
+	}
+
+	printf("%d\n", min);
+
+	return 0;
+}
