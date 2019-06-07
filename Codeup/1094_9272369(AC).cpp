a1993829ad16784500e423051051fd52374fa54b
UnluckyJung <ybook2012@gmail.com>
2019-06-07 22:40:53 +0900

    1094

---
+++ Codeup/1094_9272369(AC).cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num;
+	int num2;
+	int arr[10000] = { 0, };
+
+	scanf("%d", &num);
+
+	for (int i = 1; i <= num; i++) {
+		scanf("%d", &num2);
+		getchar();	//엔터 버퍼를 지워준다.
+		arr[i] = num2;
+	}
+
+	for (int i = num; i >= 1; i--) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
