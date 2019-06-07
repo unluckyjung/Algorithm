3394f98e7961fc56e784843f618a8a51feb4ea02
UnluckyJung <ybook2012@gmail.com>
2019-06-07 22:37:52 +0900

    1092 93

---
+++ Codeup/1092_9272111(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int n1, n2, n3;
+	int day = 1;
+	scanf("%d %d %d", &n1, &n2, &n3);
+	while (1) {
+		if (day % n1 == 0 && day%n2 == 0 && day%n3 == 0) {
+			printf("%d\n", day);
+			break;
+		}
+		day++;
+	}
+	return 0;
+}
---
+++ Codeup/1093_9272345(AC).cpp
@@ -0,0 +1,21 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int num;
+	int num2;
+	int arr[10000] = { 0, };
+
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; i++) {
+		scanf("%d", &num2);
+		getchar();	//엔터 버퍼를 지워준다.
+		arr[num2]++;
+	}
+
+	for (int i = 1; i <= 23; i++) {
+		printf("%d ", arr[i]);
+	}
+	return 0;
+}
