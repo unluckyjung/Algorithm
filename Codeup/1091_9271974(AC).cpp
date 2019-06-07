ad63faec8a8f6ee4872aa7b674aab7f4b1eb92c8
UnluckyJung <ybook2012@gmail.com>
2019-06-07 21:54:44 +0900

    등비 + 등차

---
+++ Codeup/1091_9271974(AC).cpp
@@ -0,0 +1,19 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int a, m, d, n;
+	int result;
+	scanf("%d %d %d %d", &a, &m, &d, &n);
+	if (n == 1) {
+		printf("%d\n", a);
+		return 0;
+	}
+	result = a * m + d;
+	for (int i = 2; i < n; i++) {
+		result = result * m + d;
+	}
+	printf("%d\n", result);
+	return 0;
+}
