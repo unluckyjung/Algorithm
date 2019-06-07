39d59e6500aedae294c6358abdb71909b87144b6
UnluckyJung <ybook2012@gmail.com>
2019-06-07 21:29:49 +0900

    1088

---
+++ Codeup/1088_9271766(AC).cpp
@@ -0,0 +1,17 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	int i = 0;
+	scanf("%d", &n);
+
+	while (i<n) {
+		i++;
+		if (i % 3 != 0)
+			printf("%d ", i);
+	}
+
+	return 0;
+}
+
