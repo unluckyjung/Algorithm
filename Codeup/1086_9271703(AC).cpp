d09bb2de7178ac8a1ac20b51bb9953b7619866ca
UnluckyJung <ybook2012@gmail.com>
2019-06-07 21:20:20 +0900

    1086

---
+++ Codeup/1086_9271703(AC).cpp
@@ -0,0 +1,12 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n1, n2, n3;
+	scanf("%d %d %d", &n1, &n2, &n3);
+	printf("%.2f MB", (double)n1*n2*n3/(8*pow(2,20)));
+
+
+	return 0;
+}
+
