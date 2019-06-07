d855da19d8e6a45225a0944ee975e30874e79fa0
UnluckyJung <ybook2012@gmail.com>
2019-06-07 21:27:04 +0900

    1087

---
+++ Codeup/1087_9271740(AC).cpp
@@ -0,0 +1,22 @@
+﻿#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+	int i = 0;
+	int sum = 0;
+	scanf("%d", &n);
+
+	while (1) {
+		if (sum >= n)
+			break;
+
+		sum += i;
+		i++;
+	}
+	printf("%d\n", sum);
+
+
+	return 0;
+}
+
