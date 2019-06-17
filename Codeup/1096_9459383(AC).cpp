0c3f9e6d2c0aa848d4ee6a3620c8a4661164da5b
UnluckyJung <ybook2012@gmail.com>
2019-06-17 23:19:48 +0900

    바둑판 놓기 (띄어쓰기 주의)

---
+++ Codeup/1096_9459383(AC).cpp
@@ -0,0 +1,23 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+
+	int num;
+	int x, y;
+	int arr[19][19] = { 0, };
+	
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; ++i) {
+		scanf("%d %d", &x, &y);
+		arr[x-1][y-1] = 1;
+	}
+
+	for (int i = 0; i < 19; i++) {
+		for (int j = 0; j < 19; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
