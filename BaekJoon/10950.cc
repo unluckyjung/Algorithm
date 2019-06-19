3436708dae77662899217243711f2967dcb1e230
UnluckyJung <ybook2012@gmail.com>
2019-06-19 23:29:10 +0900

    10950 / A+B - 3

---
+++ BaekJoon/10950.cc
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+	int num;
+	scanf("%d", &num);
+	
+	while(num--){
+		int n1, n2;
+		scanf("%d %d", &n1, &n2);
+		printf("%d\n", n1 + n2);
+	}
+	return 0;
+}
