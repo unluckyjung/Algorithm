f08c58c2ce702f8c39a7ff55c18044a94a039baf
UnluckyJung <ybook2012@gmail.com>
2019-06-20 22:24:29 +0900

    10953 ,로 입력 구분받기.

---
+++ BaekJoon/10953.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+int main() {
+	int n1, n2;
+	int num3;
+	scanf("%d" , &num3);
+	while (num3--) {
+		scanf("%d,%d", &n1, &n2);
+		printf("%d\n", n1 + n2);
+	}
+	return 0;
+}
