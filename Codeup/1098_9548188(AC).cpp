91256136e93cfdec44844c503779ad2600a3f3ad
UnluckyJung <ybook2012@gmail.com>
2019-06-21 17:47:53 +0900

    그냥 배열 위치 생각하면됨..

---
+++ Codeup/1098_9548188(AC).cpp
@@ -0,0 +1,39 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+
+	int arr[101][101] = { 0, };
+
+	int w, h;
+	int n;
+	int l, d, x, y;
+
+	scanf("%d %d", &h, &w);
+	scanf("%d", &n);
+
+	while (n--) {
+		scanf("%d %d %d %d", &l, &d, &x, &y);
+		if (d == 0) {
+			arr[x - 1][y - 1] = 1;
+			for (int i = 1; i < l; i++) {
+				arr[x - 1][y - 1 + i] = 1;
+			}
+		}
+		else {
+			arr[x - 1][y - 1] = 1;
+			for (int i = 1; i < l; i++) {
+				arr[x - 1 + i][y - 1] = 1;
+			}
+		}
+	}
+
+	for (int i = 0; i < h; ++i) {
+		for (int j = 0; j < w; ++j) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+
+	return 0;
+}
+
