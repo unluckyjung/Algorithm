6fccb8d87e9e590a73599230ca4113bd07ccfef5
UnluckyJung <ybook2012@gmail.com>
2019-06-21 16:18:28 +0900

    배열을 이용한 O(n) 정렬

---
+++ Codeup/3014_9545560(AC).cpp
@@ -0,0 +1,25 @@
+﻿#include<bits/stdc++.h>
+
+int main() {
+
+	int num;
+	int arr[100001] = { 0, };
+	int n1;
+
+	scanf("%d", &num);
+
+	for (int i = 0; i < num; ++i) {
+		scanf("%d", &n1);
+		arr[n1]++;
+	}
+
+	for (int i = 0; i < 100001; ++i) {
+		if (arr[i] != 0) {
+			for (int j = 0; j < arr[i]; ++j) {
+				printf("%d ", i);
+			}
+		}
+	}
+	return 0;
+}
+
