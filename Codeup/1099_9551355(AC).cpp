132a3d57e722a14fa6f45ca25feaf9065e0e4c90
UnluckyJung <ybook2012@gmail.com>
2019-06-22 04:00:48 +0900

    delete flag & wall

--- Codeup/1099_9551355(AC).cpp
+++ Codeup/1099_9551355(AC).cpp
@@ -1,42 +1,29 @@
 ﻿#include <bits/stdc++.h>
-
-
 int main()
 {
 	int arr[10][10] = { 0, };
-	int wall;
-
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
-			scanf("%d", &wall);
-			arr[i][j] = wall;
+			scanf("%d", &arr[i][j]);
 		}
 	}
-
-	
 	int x = 1, y = 1;
-	int flag = 0;
 	while (1) {
-
 		if (arr[x][y] == 1)
 			break;
-
 		if (arr[x][y] == 2) {
 			arr[x][y] = 9;
 			break;
 		}
-
-		if (arr[x][y+1] != 1) {
+		if (arr[x][y + 1] != 1) {
 			arr[x][y] = 9;
 			y++;
 		}
-		else{
+		else {
 			arr[x][y] = 9;
 			x++;
 		}
 	}
-	
-
 	for (int i = 0; i < 10; i++) {
 		for (int j = 0; j < 10; j++) {
 			printf("%d ", arr[i][j]);
@@ -45,4 +32,3 @@ int main()
 	}
 	return 0;
 }
-
