76d2a80fa8077ddf0b519dc4f11b1a8e9dc3d2df
UnluckyJung <ybook2012@gmail.com>
2019-06-22 03:53:19 +0900

    먹이를 만났을때도 먹이를 9로 만들고 break 해야함.

---
+++ Codeup/1099_9551355(AC).cpp
@@ -0,0 +1,48 @@
+﻿#include <bits/stdc++.h>
+
+
+int main()
+{
+	int arr[10][10] = { 0, };
+	int wall;
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			scanf("%d", &wall);
+			arr[i][j] = wall;
+		}
+	}
+
+	
+	int x = 1, y = 1;
+	int flag = 0;
+	while (1) {
+
+		if (arr[x][y] == 1)
+			break;
+
+		if (arr[x][y] == 2) {
+			arr[x][y] = 9;
+			break;
+		}
+
+		if (arr[x][y+1] != 1) {
+			arr[x][y] = 9;
+			y++;
+		}
+		else{
+			arr[x][y] = 9;
+			x++;
+		}
+	}
+	
+
+	for (int i = 0; i < 10; i++) {
+		for (int j = 0; j < 10; j++) {
+			printf("%d ", arr[i][j]);
+		}
+		printf("\n");
+	}
+	return 0;
+}
+
