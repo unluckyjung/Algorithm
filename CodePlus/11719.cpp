2acf76d378ec5c6b2487e89e09f3b46e7f28d7e9
UnluckyJung <ybook2012@gmail.com>
2019-08-13 16:56:08 +0900

    11719

---
+++ CodePlus/11719.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    char c;
+    while ((c = getchar()) && c != EOF)
+    {
+        printf("%c", c);
+    }
+
+    // while(scanf("%c", &c) != EOF){
+    //     printf("%c", c);
+    // }
+    return 0;
+}
