d22170e696454b87a9b514e3894354cfab26baf6
UnluckyJung <ybook2012@gmail.com>
2019-08-13 17:16:11 +0900

    11021

---
+++ CodePlus/11021.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    int t;
+    int n1, n2;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        scanf("%d %d", &n1, &n2);
+        printf("Case #%d: %d\n", i, n1 + n2);
+    }
+    return 0;
+}
