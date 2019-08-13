315b7a19627bb96cb162deaac43fc221a698f387
UnluckyJung <ybook2012@gmail.com>
2019-08-13 17:01:45 +0900

    11721

---
+++ CodePlus/11721.cpp
@@ -0,0 +1,8 @@
+#include<bits/stdc++.h>
+int main(){
+    char s[101];
+    while(scanf("%10s", s)!=EOF){   //==1 도 괜찮다.
+        printf("%s\n", s);
+    }         //%사이에 수를 넣으면 그 길이 만큼 입력을 받게 된다.
+    return 0;
+}
