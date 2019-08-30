aaf323ed27c4b778f9592dc1fcaf12b47dddc049
JungYoonSung <ybook2012@gmail.com>
2019-08-30 22:47:47 +0900

    STL : deque

---
+++ STL/deque.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    deque<int> dq;
+    dq.push_front(5);
+    dq.push_back(4);
+
+    dq.pop_front();
+    dq.pop_back();
+
+    return 0;
+}
