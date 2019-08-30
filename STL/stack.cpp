766d4ff7bec20daabee38d0c3801f3dc8bf17991
JungYoonSung <ybook2012@gmail.com>
2019-08-30 22:43:04 +0900

    STL : stack

---
+++ STL/stack.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    stack<int> st;
+    st.push(5);
+    st.pop();
+
+    st.top();
+    st.empty(); //비어있다면 1 return
+    st.size();
+    return 0;
+}
