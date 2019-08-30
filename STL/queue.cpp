20a790b03f5f6737f53c2c70969e57b242ec0e4a
JungYoonSung <ybook2012@gmail.com>
2019-08-30 22:46:13 +0900

    STL : queue

---
+++ STL/queue.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+
+    queue<int> q;
+    q.push(4);
+    q.pop();
+    
+    q.front();
+    q.back();
+
+    q.empty();  //비어있다면, return 1;
+    q.size();
+    return 0;
+}
