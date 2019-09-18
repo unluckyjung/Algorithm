4dfae50052b1519b11e00d479157f4fdbf72f583
JungYoonSung <ybook2012@gmail.com>
2019-09-18 19:33:30 +0900

    priror_queue. 우선순위 큐 Heap 가장큰 값, 작은 값

---
+++ STL/priority_queue.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	priority_queue<int> pq_up;
+	//priority_queue<int, vector<int> , less<int>> pq_up;
+	//위와 완전히 동일하다.
+
+	pq_up.push(5);
+	pq_up.push(23);
+	pq_up.push(53);
+	pq_up.push(17);
+	cout << pq_up.top() << "\n";	//	53
+
+	priority_queue<int, vector<int>, greater<int>> pq_down;
+	//greater를 주어서, 내림차순 정렬.
+
+	pq_down.push(5);
+	pq_down.push(23);
+	pq_down.push(53);
+	pq_down.push(17);
+	cout << pq_down.top() << "\n";	//	5
+    //front()가 아닌 top()이다.
+	return 0;
+}
