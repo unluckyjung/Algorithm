8c3698831d8f23c7e9acece02f158e4991782f11
JungYoonSung <ybook2012@gmail.com>
2020-01-02 17:44:55 +0900

    exit(0) 주의하기.

--- BaekJoon/3197.cc
+++ BaekJoon/3197.cc
@@ -3,7 +3,9 @@ using namespace std;
 
 char board[1501][1501];
 bool visit[1501][1501];
-bool w_visit[1501][1501];
+//bool w_visit[1501][1501];
+int w_time[1501][1501];
+bool w_used[1501][1501];
 
 int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
@@ -21,10 +23,11 @@ void input() {
         string line;    cin >> line;
         for (int ii = 0; ii < m; ++ii) {
             board[i][ii] = line[ii];
-
+            if (board[i][ii] == '.') w_time[i][ii] = 0;
             if (!find_L && board[i][ii] == 'L') {
                 L_x = i;
                 L_y = ii;
+                find_L = true;
             }
         }
     }
@@ -53,7 +56,7 @@ void can_go() {
 
         if (cur_x != L_x and cur_y != L_y and board[cur_x][cur_y] == 'L') {
             cout << cnt << "\n";
-            exit(1);
+            exit(0);
         }
 
         for (int dir = 0; dir < 4; ++dir) {
@@ -72,22 +75,24 @@ void can_go() {
 
 void sink() {
     cnt++;
-    memset(w_visit, false, sizeof(w_visit));
 
     for (int i = 0; i < n; ++i) {
         for (int ii = 0; ii < m; ++ii) {
+            if (w_used[i][ii])  continue;
             if (board[i][ii] == '.') {
-                if (w_visit[i][ii])continue;
-                w_visit[i][ii] = true;
+                if (w_time[i][ii] > cnt)continue;
+                w_used[i][ii] = true;
+                //w_visit[i][ii] = cnt;
 
                 for (int dir = 0; dir < 4; ++dir) {
                     int nx = i + dx[dir];
                     int ny = ii + dy[dir];
 
                     if (nx < 0 or nx >= n or ny < 0 or ny >= m)continue;
-                    if (w_visit[nx][ny])continue;
+                    if (w_used[nx][ny]) continue;
+                    if (w_time[nx][ny] >= cnt)continue;
                     if (board[nx][ny] != 'X')continue;
-                    w_visit[nx][ny] = true;
+                    w_time[nx][ny] = cnt+1;
 
                     board[nx][ny] = '.';
                 }
@@ -101,6 +106,8 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     freopen("input.txt", "r", stdin);
+    memset(w_time, -1, sizeof(w_time));
+
     input();
 
      while(1){
