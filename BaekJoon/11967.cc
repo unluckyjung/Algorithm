a0955fc7bf988426c1a78f9408a6d22aa61d6fe4
JungYoonSung <ybook2012@gmail.com>
2020-01-08 21:06:28 +0900

    불켜기
    lq를 비울때 조건을 잘 고려 해봤어야한다.
    불이 켜져 있지만, 가지 않은 곳을 탐색하고 , 그곳을 갈 수 있는지를 탐색할때, 근처의 곳이 방문했던 곳 인지를 확인하여 점검한다. 
    그리고 "해당 방"에서 킬 수 있는 불을 다키고
    "해당 방"을 큐에 넣어야한다.

--- BaekJoon/11967.cc
+++ BaekJoon/11967.cc
@@ -1,82 +1,101 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int x, y, a, b;
-map<pair<int, int>, queue<pair<int, int>>> room;
-queue<pair<int, int> > q;
-int cnt = 1;
-
+int board[101][101];
 bool visit[101][101];
-bool right_on[101][101];
+bool light[101][101];
+int cnt;
+
+map<pair<int, int>, queue<pair<int, int> > >light_m;
+queue<pair<int, int> > lq;
+queue<pair<int, int> > q;
 
 int dx[] = { -1,1,0,0 };
 int dy[] = { 0,0,-1,1 };
 
-void input()
-{
-    cin >> n >> m;
-    while (m--)
-    {
-        cin >> x >> y >> a >> b;
-        room[{x-1, y-1}].push({ a-1,b-1 });
-    }
+int n, m;
+
+void output() {
+	for (int i = 0; i < n; ++i) {
+		for (int ii = 0; ii < n; ++ii) {
+			cout << visit[i][ii];
+		}
+		cout << "\n";
+	}
 }
 
-void turn_on(int x, int y) {
-    auto cur_room = room[{x, y}];
-    while (!cur_room.empty()) {
-        auto next_room = cur_room.front();
-        cur_room.pop();
-        right_on[next_room.first][next_room.second] = true;
-    }
+void light_on(pair<int, int> k) {
+	while (!light_m[k].empty()) {
+		auto cur = light_m[k].front();	light_m[k].pop();
+		if (!light[cur.first][cur.second]) {
+			light[cur.first][cur.second] = true;
+			lq.push({ cur.first, cur.second });
+			cnt++;
+		}
+	}
 }
 
 void bfs() {
-    while (!q.empty()) {
-        auto cur = q.front();
-        q.pop();
-        cnt++;
+	if (q.empty())return;
 
-        for (int dir = 0; dir < 4; ++dir) {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
-            if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
-            if (visit[nx][ny])continue;
-            if (!right_on[nx][ny])continue;
-            visit[nx][ny] = true;
-            turn_on(nx, ny);
-            q.push({ nx,ny });
-        }
-    }
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+		//light_on(cur);
+
+		for (int dir = 0; dir < 4; ++dir) {
+			int nx = cur.first + dx[dir];
+			int ny = cur.second + dy[dir];
+			if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
+			if (visit[nx][ny])continue;
+			if (!light[nx][ny])continue;
+			visit[nx][ny] = true;
+			light_on({ nx,ny });
+			q.push({ nx,ny });
+		}
+	}
+	while (!lq.empty()) {
+		auto cur = lq.front();	lq.pop();
+		if (visit[cur.first][cur.second])continue;
+
+		for (int dir = 0; dir < 4; ++dir) {
+			int nx = cur.first + dx[dir];
+			int ny = cur.second + dy[dir];
+			if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
+			if (visit[nx][ny]) {
+				visit[cur.first][cur.second] = true;
+				light_on({ cur.first,cur.second });
+				q.push({ cur.first, cur.second });
+				break;
+			}
+		}
+	}
+	bfs();
 }
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
 
-void output() {
-    for (int i = 0; i < n; ++i) {
-        for (int ii = 0; ii < n; ++ii) {
-            //if (visit[i][ii]) cout << "1";
-            //else cout << "0";
-            cout << right_on[i][ii];
-        }
-        cout << "\n";
-    }
-}
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    /*제출전 주석처리*/ freopen("input.txt", "r", stdin);
-    cin.tie(NULL);
+	freopen("input.txt", "r", stdin);
 
-    input();
+	cin >> n >> m;
+	while (m--) {
+		int a, b, c, d;
+		cin >> a >> b >> c >> d;
+		light_m[{a - 1, b - 1}].push({ c - 1,d - 1 });
+	}
 
-    visit[0][0] = true;
-    right_on[0][0] = true;
-    turn_on(0, 0);
-    q.push({0,0 });
+	visit[0][0] = true;
+	light[0][0] = true;
 
-    bfs();
+	light_on({ 0,0 });
+	lq.push({ 0,0 });
+	q.push({ 0,0 });
+	cnt++;
 
-    output();
-    cout << cnt <<"\n";
-    return 0;
+	bfs();
+
+	//output();
+	cout << cnt;
+
+	return 0;
 }
