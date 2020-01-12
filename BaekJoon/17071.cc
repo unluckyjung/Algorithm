1748a007c8ea9062759ad7f36fbe365fa8c5530b
JungYoonSung <ybook2012@gmail.com>
2020-01-12 22:10:54 +0900

    숨바꼭질 5
    왜맞틀

--- BaekJoon/17071.cc
+++ BaekJoon/17071.cc
@@ -1,64 +1,89 @@
-//#include<bits/stdc++.h>
-//using namespace std;
-//
-//int board[500001];
-//
-//queue<int> q;
-//int cnt;
-//int n, k;
-//
-//int subin, sister;
-//int day;
-//
-//
-//void sister_move() {
-//	day++;
-//	k += day;
-//	cout << k << " ";
-//	if (k > 500000) {
-//		cout << -1 << "\n";
-//		exit(0);
-//	}
-//}
-//
-//void bfs() {
-//	memset(board, -1, sizeof(board));
-//	subin = n, sister = k;
-//	board[subin] = 0;
-//
-//	q.push(subin);
-//
-//	while (!q.empty()) {
-//		int cur = q.front(); q.pop();
-//		if (cur == sister && board[cur] == day) {
-//			cout << board[cur] << "\n";
-//			exit(0);
-//		}
-//
-//		int arr[] = { cur + 1, cur - 1 ,cur * 2 };
-//		for (int dir = 0; dir < 3; ++dir) {
-//			int nx = arr[dir];
-//			if (nx < 0 or nx >500000)continue;
-//			if (board[nx] != -1)continue;
-//			if (board[cur] + 1 > day)continue;
-//			board[nx] = board[cur] + 1;
-//			q.push(nx);
-//		}
-//	}
-//
-//	sister_move();
-//	bfs();
-//}
-//
-//int main() {
-//	ios_base::sync_with_stdio(false);
-//	cin.tie(NULL);
-//
-//	freopen("input.txt", "r", stdin);
-//	//freopen("output.txt", "w", stdout);
-//
-//	cin >> n >> k;
-//	bfs();
-//
-//	return 0;
-//}
+#include<bits/stdc++.h>
+using namespace std;
+
+int board[500001][2];
+
+queue<pair<int, int>> q;
+int cnt;
+int n, k;
+
+int subin, sister;
+int day;
+
+
+void sister_move() {
+	while (1) {
+		day++;
+		k += day;
+
+		if (k > 500000) {
+			cout << -1 << "\n";
+			exit(0);
+		}
+
+		if (board[k][0] == -1 && board[k][1] == -1)continue;
+		if (board[k][0] > day&& board[k][1] > day)continue;
+
+		if (board[k][0] == day or board[k][1] == day) {
+			cout << day << "\n";
+			exit(0);
+		}
+
+		if (day % 2 == 0 && board[k][0] != -1) {
+			cout << max(day,board[k][0]) << "\n";
+			exit(0);
+		}
+
+		if (day % 2 != 0 && board[k][1] != -1) {
+			cout << max(day, board[k][1]) << "\n";
+			exit(0);
+		}
+
+	}
+}
+
+void bfs() {
+	while (!q.empty()) {
+		auto cur = q.front(); q.pop();
+		int cur_dx = cur.first;
+		int cur_day = cur.second;
+		int arr[] = { cur_dx - 1,cur_dx + 1, cur_dx * 2 };
+		for (int dir = 0; dir < 3; ++dir) {
+			int nx = arr[dir];
+			if (nx < 0 or nx >500000)continue;
+			if ((cur_day + 1) % 2 == 0) {
+				if (board[nx][0] != -1)continue;
+				board[nx][0] = cur_day + 1;
+				q.push({ nx, cur_day + 1 });
+			}
+			else {
+				if (board[nx][1] != -1)continue;
+				board[nx][1] = cur_day + 1;
+				q.push({ nx, cur_day + 1 });
+			}
+		}
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	freopen("input.txt", "r", stdin);
+	//freopen("output.txt", "w", stdout);
+
+	cin >> n >> k;
+	if (n == k) {
+		cout << 0 << "\n";
+		return 0;
+	}
+
+	memset(board, -1, sizeof(board));
+	board[n][0] = 0;
+	board[n][1] = 0;
+	q.push({ n , 0 });
+	bfs();
+	sister_move();
+
+	return 0;
+}
