6a5ec702202d54ce6bf79d50ef3a19e895dc491e
JungYoonSung <ybook2012@gmail.com>
2020-01-13 13:02:01 +0900

    숨바꼭질5 solved
    짝수, 홀수 시간대로 구별해야 함.
    그리고 시작점의 경우 (0초)의 홀수 부분은 -1로 되어 있어야함. 
    이부분을 고려하지 않을경우 엣지케이스에서 걸림 (99% 오답)

--- BaekJoon/17071.cc
+++ BaekJoon/17071.cc
@@ -1,65 +1,54 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int board[500001][2];
-
-queue<pair<int, int>> q;
-int cnt;
-int n, k;
-
-int subin, sister;
 int day;
-
+int n, k;
+int board[500001][2];
+queue<pair<int, int > > q;
 
 void sister_move() {
 	while (1) {
 		day++;
 		k += day;
-
 		if (k > 500000) {
 			cout << -1 << "\n";
 			exit(0);
 		}
 
-		if (board[k][0] == -1 && board[k][1] == -1)continue;
-		if (board[k][0] > day&& board[k][1] > day)continue;
-
-		if (board[k][0] == day or board[k][1] == day) {
+		if (day % 2 == 0) {
+			if (board[k][0] == -1 or board[k][0] > day)continue;
+			//cout << k << "\n";
+			//cout << board[k][0] << "\n";
 			cout << day << "\n";
 			exit(0);
 		}
-
-		if (day % 2 == 0 && board[k][0] != -1) {
-			cout << max(day,board[k][0]) << "\n";
+		else {
+			if (board[k][1] == -1 or board[k][1] > day)continue;
+			cout << day << "\n";
 			exit(0);
 		}
-
-		if (day % 2 != 0 && board[k][1] != -1) {
-			cout << max(day, board[k][1]) << "\n";
-			exit(0);
-		}
-
 	}
 }
 
 void bfs() {
 	while (!q.empty()) {
-		auto cur = q.front(); q.pop();
-		int cur_dx = cur.first;
-		int cur_day = cur.second;
-		int arr[] = { cur_dx - 1,cur_dx + 1, cur_dx * 2 };
+		auto cur = q.front();	q.pop();
+		int cur_nx = cur.first, next_day = cur.second + 1;
+
+		int arr[] = { cur_nx - 1, cur_nx + 1, cur_nx * 2 };
 		for (int dir = 0; dir < 3; ++dir) {
 			int nx = arr[dir];
-			if (nx < 0 or nx >500000)continue;
-			if ((cur_day + 1) % 2 == 0) {
+			if (nx < 0 or nx > 500000)continue;
+			
+			if (next_day % 2 == 0) {
 				if (board[nx][0] != -1)continue;
-				board[nx][0] = cur_day + 1;
-				q.push({ nx, cur_day + 1 });
+				board[nx][0] = next_day;
+				q.push({ nx, next_day });
 			}
 			else {
 				if (board[nx][1] != -1)continue;
-				board[nx][1] = cur_day + 1;
-				q.push({ nx, cur_day + 1 });
+				board[nx][1] = next_day;
+				q.push({ nx, next_day });
 			}
 		}
 	}
@@ -71,17 +60,16 @@ int main() {
 
 	freopen("input.txt", "r", stdin);
 	//freopen("output.txt", "w", stdout);
-
 	cin >> n >> k;
 	if (n == k) {
 		cout << 0 << "\n";
 		return 0;
 	}
-
 	memset(board, -1, sizeof(board));
 	board[n][0] = 0;
-	board[n][1] = 0;
-	q.push({ n , 0 });
+	//board[n][1] = 0;
+	q.push({ n, 0 });
+
 	bfs();
 	sister_move();
 
