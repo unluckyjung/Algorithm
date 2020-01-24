7b17fb61f431580826eae435d298f26c5f55762d
JungYoonSung <ybook2012@gmail.com>
2020-01-25 01:33:03 +0900

    1330 의미없는 문제
    solved 에서 뱃지 준다길래 품

---
+++ BaekJoon/1330.cc
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+int a, b;
+int main() {
+	cin >> a >> b;
+
+	if (a > b)	cout << ">\n";
+	else if (a < b)	cout << "<\n";
+	else cout << "==\n";
+	return 0;
+}
