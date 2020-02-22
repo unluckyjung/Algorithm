cb6cdd96e5489aae8ed168878a00700202f07f92
JungYoonSung <ybook2012@gmail.com>
2020-02-22 16:24:17 +0900

    17103 골드바흐 파티션
    [에라토스 테네스의 체]
    
    1. 소수를 구한다.
    2. 소수의 순서만 다른 것은 같은 파티션이기 때문에
    절반 만큼만 탐색한다.
    10의 경우
    ( 3, 7 ), ( 5, 5 ) 가 있고 (7 , 3)의 경우에는 (3, 7)과 같다.
    더해지는 첫 번째 숫자의 범위를, 합의 절반 이하로 잡으면 된다.

---
+++ BaekJoon/17103.cc
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+#define MAX 1000001
+
+int visit[MAX], t;
+using namespace std;
+
+void prime_set() {
+    for (int i = 2; i < MAX; ++i) {
+        for (int ii = i; ii < MAX; ii += i) {
+            visit[ii]++;
+        }
+    }
+}
+
+int solve(int n) {
+    int patition = 0;
+    for (int i = 3; i <= n / 2; i += 2) {
+        if (visit[i] != 1 or visit[n - i] != 1)continue;
+        patition++;
+    }
+    return patition;
+}
+
+void input() {
+    cin >> t;
+    int n;
+    while (t--) {
+        cin >> n;
+        cout << solve(n) << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    prime_set();
+    input();
+    return 0;
+}
