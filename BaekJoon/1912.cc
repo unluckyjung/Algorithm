1ac4940ecd577aff3bba0eed6ecd50e68fb30e83
JungYoonSung <ybook2012@gmail.com>
2020-02-24 22:46:26 +0900

    1912 연속합
    [DP]
    
    각 인덱스 까지의 연속합 중 가장 큰 것을 찾는다
    dp[n]은 n번 인덱스까지 더했을 때, 최대로 큰 수
    
    앞에서 계산한 것을 반복 할 필요가 없게 된다.
    
    주의 할 점은 dp[1]부터 찾게 되므로, dp[0]을 초기 값으로 잡는다.

--- BaekJoon/1912.cc
+++ BaekJoon/1912.cc
@@ -1,34 +1,32 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define MAX 100001
 using namespace std;
-//백준 연속합 1912
-//https://www.acmicpc.net/problem/1912
 
-int arr[100005];
-int ans[100005];
-//ans[1] 1항을 마지막으로 사용하는 수열의 합 중 최댓값.
-//ans[3] 3항을 마지막으로 사용하는 수열의 합 중 최댓값
-
-//ans[k] = max(0, ans[k-1]) + arr[k];
-
+int dp[MAX];
 int n;
 
-int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+int solve() {
+    int answer = dp[0];
+    
+    for (int i = 1; i < n; ++i) {
+        dp[i] = max(dp[i], dp[i - 1] + dp[i]);
+        answer = max(answer, dp[i]);
+    }
+    return answer;
+}
 
-	cin >> n;
-	for (int i = 1; i <= n; ++i) {
-		cin >> arr[i];
-	}
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; ++i)   cin >> dp[i];
+}
 
-	ans[1] = arr[1];
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
 
-	for (int i = 2; i <= n; ++i) {
-		ans[i] = max(ans[i - 1], 0) + arr[i];
-		//ans[i-1]과 0중 큰것을 선택한다?
-		//0이하인경우 그냥 
-	}
-
-	return 0;
+    input();
+    cout << solve();
+    return 0;
 }
