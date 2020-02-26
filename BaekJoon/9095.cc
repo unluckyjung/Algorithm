01e35d5e2f6ebebc9e7a9c562334438c7f117f25
JungYoonSung <ybook2012@gmail.com>
2020-02-26 16:21:01 +0900

    9095 1,2,3 더하기
    [DP]
    
    dp[n] = n을 1,2,3의 합 으로 나타내는 방법의 수
    
    dp[1 ~ 3]은 초기값 설정.
    dp[n]은 n-1을 만드는 방법 + n-2 을 만드는 방법 + n-3을 만드는 방법, 왜냐하면 n-1을 만드는 방법에서 1을 한번 더하면 답이기 때문.
    
    dp[n] = dp[n-1] + dp[n-2] + dp[n-3]

--- BaekJoon/9095.cc
+++ BaekJoon/9095.cc
@@ -1,3 +1,4 @@
+#if 00
 #include <bits/stdc++.h>
 using namespace std;
 int t;
@@ -32,3 +33,42 @@ int main()
 
     return 0;
 }
+
+#else
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int t;
+int dp[11] = { 0,1,2,4 };
+
+void solve() {
+    for (int i = 4; i <= 10; ++i) {
+        dp[i] = dp[i - 1] + dp[i - 2] + dp[i - 3];
+    }
+}
+
+void input() {
+    cin >> t;
+
+    solve();
+
+    int num;
+    while (t--) {
+        cin >> num;
+        cout << dp[num] << "\n";
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    input();
+    return 0;
+}
+
+#endif
