9fd0a7d23a94c7c5b3d99b1ca35ce79dcf9bb2aa
JungYoonSung <ybook2012@gmail.com>
2020-02-26 15:50:49 +0900

    11727 2xn 타일링 2
    [DP]
    
    큰 문제를 작은 문제로 나누어서 푼다.
    dp[n]은 2 x n 을 채우는 타일의 수를 나타낸다.
    
    dp[n]은 끝을 n 으로 하는 타일을 놓을 때
    앞에 몇 개의 타일이 들어갔는지 확인 하면 된다.
    
    dp[n-1] , dp[n-2] , dp[n-2] 의 경우를 다 따지면 된다.

---
+++ BaekJoon/11727.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int dp[1001];
+int n, mod = 10007;
+
+int solve(int n) {
+    for (int i = 2; i <= n; ++i) {
+        dp[i] = (dp[i - 1] + dp[i - 2] * 2) % mod;
+    }
+    return dp[n];
+}
+
+void input() {
+    dp[0] = 1;
+    dp[1] = 1;
+    cin >> n;
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    input();
+    cout << solve(n);
+    return 0;
+}
