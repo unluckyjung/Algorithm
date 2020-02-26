1d6e7ced1f69121744829f077cdc7ba5d07d3d6c
JungYoonSung <ybook2012@gmail.com>
2020-02-26 12:42:07 +0900

    14888 연산자 끼워넣기
    [완탐]
    
    더하거나, 더하지 않거나 다 따져 보면 된다.
    시간 복잡도는 4^10이다.

---
+++ BaekJoon/14888.cc
@@ -0,0 +1,61 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int arr[12], oper[4];
+int n;
+int MAX = INT_MIN, MIN = INT_MAX;
+
+void dfs(int cur, int cnt) {
+
+    if (cnt == n) {
+        MAX = max(MAX, cur);
+        MIN = min(MIN, cur);
+        return;
+    }
+
+    int tmp = cur;
+
+    if (oper[0]) {
+        oper[0]--;
+        dfs(cur + arr[cnt], cnt + 1);
+        oper[0]++;
+    }
+
+    if (oper[1]) {
+        oper[1]--;
+        dfs(cur - arr[cnt], cnt + 1);
+        oper[1]++;
+    }
+
+    if (oper[2]) {
+        oper[2]--;
+        dfs(cur * arr[cnt], cnt + 1);
+        oper[2]++;
+    }
+
+    if (oper[3]) {
+        oper[3]--;
+        dfs(cur / arr[cnt], cnt + 1);
+        oper[3]++;
+    }
+}
+
+void solve() {
+    dfs(arr[0], 1);
+    cout << MAX << "\n" << MIN << "\n";
+}
+
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; ++i)   cin >> arr[i];
+    for (int i = 0; i < 4; ++i)   cin >> oper[i];
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
+    input();
+    solve();
+    return 0;
+}
