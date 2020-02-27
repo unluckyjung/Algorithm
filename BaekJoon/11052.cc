e415d6c3d4a35e40487512d92c2e7687787dc456
JungYoonSung <ybook2012@gmail.com>
2020-02-27 18:20:25 +0900

    11052 16194 카드 구매하기
    [DP]
    
    dp[n]은 n개의 카드를 구매할 때의 최대, 최솟값이다.
    
    몇 개를 구매할 때, 최대 , 최소값이 되는지 모르므로, 전부 다해 보면 된다. 어차피 1000^2 이다.
    
    dp[3]의 경우, 
    3개짜리 팩을 사는 것 과
    dp[2]개 짜리 팩을 사는것 + dp[1]개 짜리 팩을 사는것을 비교해보면된다.
    
    n 개의 카드를 구매시
    다른 팩을, 최대한 구매해보고, 원하는 개수가 안 나오면, 나머지팩을 추가로 구매해 n개를 맞추었다.
    
    tmp는 구매할 팩의 번호가 된다.

---
+++ BaekJoon/11052.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+int dp[1001];
+
+int solve() {
+    for (int i = 2; i <= n; ++i) {
+        for (int ii = 0; ii < i; ++ii) {
+            int tmp = i - ii;
+            dp[i] = max(dp[i], dp[tmp] * (i / tmp) + dp[i % tmp]);
+        }
+    }
+    return dp[n];
+}
+
+void input() {
+    cin >> n;
+    for (int i = 1; i <= n; ++i)   cin >> dp[i];
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    //freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
+
+    input();
+    cout << solve();
+    return 0;
+}
