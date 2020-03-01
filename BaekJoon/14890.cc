b676719c55a5aab480ac033849fa637ae7b9d318
JungYoonSung <ybook2012@gmail.com>
2020-03-02 02:00:56 +0900

    14890 경사로
    [시뮬, 발상]
    
    쉽게 문제를 풀기 위해, 
    단 방향으로만 탐색하게 함.
    
    기존의 board를 시계 방향으로 90도 돌린 후 아래에 붙임.
    
    연속되는 평지의 개수를 계속 세어나감.
    
    높이가 두 칸 이상 차이 나면 불가능.
    
    오르막길인 경우
    평지가 len개 이상 있어야 함.
    성공한다면, 평지는 1개부터 시작.
    
    내리막길인 경우
    이후에 평지가 len개 이상 있어야 함
    평지가 있다고 가정하고 len -1 의 연속된 평지의 개수가 있다고 설정함.
    
    만약 내리막길을 만났는데,
    현재 연속된 평지가 0보다 작다는 이야기는 지금 내리막길을 만들며 내려가는 중임.
    
    내리막길은 겹칠 수 없으므로 건설 실패.

---
+++ BaekJoon/14890.cc
@@ -0,0 +1,71 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int board[202][202];
+int n, len;
+int answer;
+
+void solve() {
+    for (int i = 0; i < n * 2; ++i) {
+        int before = board[i][0];
+        int flat_cnt = 0;
+        bool flag = true;
+
+        for (int ii = 0; ii < n; ++ii) {
+            if (board[i][ii] == before) flat_cnt++;
+
+            else if (abs(board[i][ii] - before) >= 2) {
+                flag = false;
+                break;
+            }
+
+            else if (before > board[i][ii]) {
+                if (flat_cnt < 0) {
+                    flag = false;
+                    break;
+                }
+
+                before = board[i][ii];
+                flat_cnt = 1 - len;
+            }
+
+            else if (before < board[i][ii]) {
+                if (flat_cnt < len) {
+                    flag = false;
+                    break;
+                }
+
+                before = board[i][ii];
+                flat_cnt = 1;
+            }
+        }
+        if (flag) {
+            if (flat_cnt >= 0)answer++;
+        }
+    }
+
+    cout << answer << "\n";
+}
+
+void input() {
+    cin >> n >> len;
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii)   cin >> board[i][ii];
+    }
+
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii) {
+            board[n + i][ii] = board[ii][i];
+        }
+    }
+}
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    freopen("input.txt", "r", stdin);
+    cin.tie(NULL);  cout.tie(NULL);
+    input();
+    solve();
+
+    return 0;
+}
