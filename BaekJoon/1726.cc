6c5288692f88ced14cfdb0346811cb07eb18b51b
JungYoonSung <ybook2012@gmail.com>
2020-03-07 23:45:21 +0900

    1726 로봇
    [BFS]
    재 채점 후 오답 처리되어 다시 풀었다.
    
    문제에서 시작 점은 0이 아니라 1부터 시작인 것에 주의한다.
    방향의 정보도 같이 보관해야 하므로, 3차원 배열을 이용해야 한다.
    큐 안에는 좌표, 방향, 명령의 개수를 넣어주었다.
    
    회전 때문에 방향을 굳이 재 설정할 필요 없다. 
    북쪽이나 남쪽을 보고 있었으면,
    명령어 카운트를 한번 더 증가 시키고, 
    해당 좌표에서 동쪽 , 서쪽을 바라보는 방향 두 개 다 큐에 넣으려고 시도하면 된다.
    
    동쪽과 서쪽의 경우에도
    북쪽, 서쪽을 바라보는 방향을 넣어주면 된다.
    
    이동 거리의 차를 이용해서 할 필요도 없고 그렇게 해선 안된다. (재 채점에서 오답 처리 되어짐)
    
    이전에 오답 처리되었던 이유는, 하나의 단계 에서 2칸씩 돌리는 작업을 해서, 우선순위가 꼬였었다.
    
    우선 순위 큐가 아니라, 단순히 큐를 이용한 BFS이기 때문에 하나의 단계 에서 명령어는 1번씩 증가하게 했어야 했다.
    
    메모리 초과가 한번 발생했었는데, 회전 처리 부분에서 재 방문 여부를 확인하지 않고 push했기 때문이다.

--- BaekJoon/1726.cc
+++ BaekJoon/1726.cc
@@ -1,110 +1,103 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m;
-int dist[101][101][4];
-bool rail[101][101];
-
-int f_x, f_y, f_dir;
-
-int dx[] = { -1,0,1,0 };
-int dy[] = { 0,1,0,-1 };
-//  북 , 동 , 남, 서
-//  + 시계방향 , - 반시계방향
-
-int answer = INT_MAX;
-
-struct robot {
+struct Robot {
     int x, y;
-    int dir;
+    int dir, cnt;
 };
 
-queue<robot> q;
+const int dx[] = { 0,0,0,1,-1 };
+const int dy[] = { 0,1,-1,0,0 };
+
+int board[103][103];
+bool visit[103][103][5];
+
+queue<Robot> q;
+
+int n, m;
+int end_x, end_y, end_dir;
+int answer;
+
+void go_straight(Robot &cur) {
+    int nx, ny;
+    for (int move_cnt = 1; move_cnt <= 3; ++move_cnt) {
+        nx = cur.x + dx[cur.dir] * move_cnt;
+        ny = cur.y + dy[cur.dir] * move_cnt;
+
+        if (nx < 1 or nx > n or ny < 1 or ny > m)return;
+        if (board[nx][ny] == 1)return;
+
+        if (visit[nx][ny][cur.dir])continue;
+
+        visit[nx][ny][cur.dir] = true;
+        q.push({ nx,ny,cur.dir, cur.cnt + 1 });
+    }
+}
+
+void rotate(Robot &cur) {
+
+	if (cur.dir <= 2) {
+		for (int n_dir = 3; n_dir <= 4; ++n_dir) {
+            if (visit[cur.x][cur.y][n_dir])continue;
+			visit[cur.x][cur.y][n_dir] = true;
+			q.push({ cur.x, cur.y, n_dir, cur.cnt + 1 });
+		}
+	}
+	else {
+        for (int n_dir = 1; n_dir <= 2; ++n_dir) {
+            if (visit[cur.x][cur.y][n_dir])continue;
+            visit[cur.x][cur.y][n_dir] = true;
+            q.push({ cur.x, cur.y, n_dir, cur.cnt + 1 });
+        }
+	}
+}
 
 
-int change_real_dir(int n) {
-    if (n == 1)return 1;
-    else if (n == 2) return 3;
-    else if (n == 3) return 2;
-    else if (n == 4) return 0;
+int bfs() {
+    while (!q.empty()) {
+        Robot cur = q.front(); q.pop(); 
+
+        if (cur.x == end_x && cur.y == end_y && cur.dir == end_dir) {            
+            return cur.cnt;
+        }
+
+        go_straight(cur);
+        rotate(cur);
+    }
+}
+
+void init() {
+    int s_x, s_y, s_d;
+    cin >> s_x >> s_y >> s_d;
+
+    visit[s_x][s_y][s_d] = true;
+    q.push({ s_x, s_y, s_d, 0 });
+
+    cin >> end_x >> end_y >> end_dir;
+}
+
+void solve() {
+    cout << bfs() << "\n";
 }
 
 void input() {
-    memset(dist, -1, sizeof(dist));
-
     cin >> n >> m;
-    for (int i = 0; i < n; ++i) {
-        for (int ii = 0; ii < m; ++ii) {
-            cin >> rail[i][ii];
+    for (int i = 1; i <= n; ++i) {
+        for (int ii = 1; ii <= m; ++ii) {
+            cin >> board[i][ii];
         }
     }
-
-    robot s_robot;
-    int dir;
-
-    cin >> s_robot.x >> s_robot.y >> dir;
-
-    s_robot.x--, s_robot.y--;
-
-    s_robot.dir = change_real_dir(dir);
-    dist[s_robot.x][s_robot.y][s_robot.dir] = 0;
-    q.push(s_robot);
-
-    cin >> f_x >> f_y >> f_dir;
-    f_x--, f_y--;
-    f_dir = change_real_dir(f_dir);
+    init();
 }
 
-
-void solve() {
-    while (!q.empty()) {
-        robot cur = q.front(); q.pop();
-        if (cur.x == f_x && cur.y == f_y) {
-            if (cur.dir == f_dir) answer = dist[f_x][f_y][cur.dir];
-            else {
-                int ret = abs(cur.dir - f_dir);
-                answer = dist[f_x][f_y][cur.dir] + ret;
-            }
-            return;
-        }
-
-        for (int i = 1; i <= 3; ++i) {
-            int nx = cur.x + dx[cur.dir] * i;
-            int ny = cur.y + dy[cur.dir] * i;
-            if (nx < 0 or nx >= n or ny < 0 or ny >= m)break;
-            if (rail[nx][ny])break;
-            if (dist[nx][ny][cur.dir] != -1)continue;   //이미 갔었던곳이면 넘어갈뿐.
-            dist[nx][ny][cur.dir] = dist[cur.x][cur.y][cur.dir] + 1;
-
-            robot tmp;
-            tmp.x = nx, tmp.y = ny, tmp.dir = cur.dir;
-            q.push(tmp);
-        }
-
-        for (int rot = 1; rot <= 3; ++rot) {
-            int next_rot = cur.dir + rot;
-            next_rot %= 4;
-            if (dist[cur.x][cur.y][next_rot] != -1)continue;
-
-            if (rot == 2) dist[cur.x][cur.y][next_rot] = dist[cur.x][cur.y][cur.dir] + 2;
-            else dist[cur.x][cur.y][next_rot] = dist[cur.x][cur.y][cur.dir] + 1;
-
-            robot tmp;
-            tmp.x = cur.x, tmp.y = cur.y, tmp.dir = next_rot;
-            q.push(tmp);
-        }
-    }
-}
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
+    cin.tie(NULL);  cout.tie(NULL);
     freopen("input.txt", "r", stdin);
 
     input();
     solve();
-    cout << answer;
 
     return 0;
 }
