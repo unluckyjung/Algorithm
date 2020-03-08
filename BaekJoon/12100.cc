f97e69b0fee6dc8a81c2dba57f12f3a0c88e2e53
JungYoonSung <ybook2012@gmail.com>
2020-03-08 16:52:12 +0900

    12100 2048(easy) 소스 간략화
    움직이는 방향을 각각 따로 케이스별로 시도하지 않고,
    게임판 자체를 돌리고, 한쪽 방향으로만  움직이게 함.
    
    실수한 부분 : deque에 넣는 과정에서 큐가 비지 않았을때 동일 데이터 여부, 합침 여부를 확인하고 합침이 불가능 할때 의 처리를 제대로 안 했었음.

--- BaekJoon/12100.cc
+++ BaekJoon/12100.cc
@@ -1,3 +1,108 @@
+#if 01
+
+#include <bits/stdc++.h>
+#define pp pair<bool, int>
+#define merge first
+#define data second
+using namespace std;
+
+int board[21][21];
+int n;
+int answer;
+
+void move(int i) {
+    deque<pp> q;
+
+    for (int ii = 0; ii < n; ++ii) {
+        if (board[i][ii] == 0)continue;
+        
+        if (!q.empty()) {
+            if (q.back().merge == false && q.back().data == board[i][ii]) {
+                q.pop_back();
+                q.push_back({ true, board[i][ii] * 2 });
+                continue;
+            }
+        }
+        q.push_back({ false, board[i][ii] });
+    }
+    
+    while (q.size() < n) {
+        q.push_back({ false, 0 });
+    }
+
+    for (int ii = 0; ii < n; ++ii) {
+        board[i][ii] = q.front().data;  q.pop_front();
+    }
+}
+
+void rotate(int k) {
+	int backup_board[21][21];
+
+	while (k--) {
+		memcpy(backup_board, board, sizeof(board));
+
+		for (int i = 0; i < n; ++i) {
+			for (int ii = 0; ii < n; ++ii) {
+				board[i][ii] = backup_board[(n - 1) - ii][i];
+			}
+		}
+	}
+
+}
+
+void find_max() {
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii)
+            answer = max(answer, board[i][ii]);
+    }
+}
+
+void dfs(int move_cnt) {
+    if (move_cnt == 5) {
+        find_max();
+        return;
+    }
+
+    int backup_board[21][21];
+    memcpy(backup_board, board, sizeof(board));
+
+    for (int k = 0; k < 4; ++k) {
+        rotate(k);
+        for (int i = 0; i < n; ++i) move(i);
+
+        dfs(move_cnt + 1);
+        memcpy(board, backup_board, sizeof(board));
+    }
+}
+
+void solve() {
+    dfs(0);
+    cout << answer << "\n";
+}
+
+void input() {
+    cin >> n;
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii)  cin >> board[i][ii];
+    }
+}
+
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    input();
+    solve();
+
+    return 0;
+}
+
+#else
+
+
 #include <bits/stdc++.h>
 #define pp pair<int, bool>
 using namespace std;
@@ -112,3 +217,5 @@ int main()
     cout << answer;
     return 0;
 }
+
+#endif
