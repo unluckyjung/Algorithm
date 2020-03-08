3ccdc67b71f00dffa7db4112e187fc65da27a000
JungYoonSung <ybook2012@gmail.com>
2020-03-08 17:47:34 +0900

    14980 경사로 소스 간략화

--- BaekJoon/14890.cc
+++ BaekJoon/14890.cc
@@ -1,3 +1,76 @@
+#if 01
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int n, runway_len;
+int board[204][102];
+
+bool go_check(int i) {
+    int before_h = board[i][0];
+    int flat_cnt = 1;
+
+    for (int ii = 1; ii < n; ++ii) {
+        int cur_h = board[i][ii];
+
+        if (before_h == cur_h) flat_cnt++;
+        else if (abs(before_h - cur_h) > 1) return false;
+
+        else if (before_h < cur_h) {
+            if (flat_cnt < runway_len)  return false;
+            flat_cnt = 1;
+        }
+
+        else if (before_h > cur_h) {
+            if (flat_cnt < 0)   return false;
+            flat_cnt = 1 - runway_len;
+        }
+
+        before_h = cur_h;
+    }
+
+    if (flat_cnt < 0)return false;
+    else return true;
+}
+
+int solve() {
+    int way_cnt = 0;
+    for (int i = 0; i < n * 2; ++i) {
+        if (go_check(i))    way_cnt++;
+    }
+    return way_cnt;
+}
+
+void init() {
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii) {
+            board[n + i][ii] = board[(n - 1) - ii][i];
+        }
+    }
+}
+
+void input() {
+    cin >> n >> runway_len;
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii)  cin >> board[i][ii];
+    }
+    init();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    input();
+    cout << solve();
+
+    return 0;
+}
+
+#else
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -69,3 +142,5 @@ int main()
 
     return 0;
 }
+
+#endif
