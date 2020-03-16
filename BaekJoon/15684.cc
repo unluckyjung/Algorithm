af698b703db4a5aeb5288b8ea2c5b9538ba0a877
JungYoonSung <ybook2012@gmail.com>
2020-03-16 20:50:33 +0900

    15684 사다리조작 2회차
    [DFS]
    
    사다리를 놓아보는 for문에서
    b의 범위 n-1을 n으로 쓰는 실수를 했다.
    
    사다리는 최대 3개인데,
    cur > 4 로 잘못 써서 한번 틀렸다.

--- BaekJoon/15684.cc
+++ BaekJoon/15684.cc
@@ -1,3 +1,84 @@
+#if 01
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bool ladder[32][12];
+
+int n, m, h;
+int answer = INT_MAX;
+
+bool check() {
+    for (int ladder_num = 1; ladder_num <= n; ++ladder_num) {
+        int cur_spot = ladder_num;
+        for (int height = 1; height <= h; ++height) {
+            if (ladder[height][cur_spot])  cur_spot++;
+            else if (ladder[height][cur_spot - 1]) cur_spot--;
+        }
+        if (cur_spot != ladder_num) return false;
+    }
+    return true;
+}
+
+bool add_ladder(int height, int ladder_num) {
+    for (int i = -1; i <= 1; ++i) {
+        if (ladder[height][ladder_num + i]) return false;
+    }
+    return true;
+}
+
+
+void dfs(int height, int ladder_num, int cur) {
+    if (cur >= answer || cur > 3) return;
+    if (check()) {
+        answer = cur;
+        return;
+    }
+
+	for (int a = height; a <= h; ++a) {
+		for (int b = ladder_num; b <= n-1; ++b) {
+
+			if (!add_ladder(a, b))continue;
+			ladder[a][b] = true;
+			dfs(a, b + 1, cur + 1);
+			ladder[a][b] = false;
+
+		}
+		ladder_num = 1;
+	}
+
+}
+
+void solve() {
+    dfs(1, 1, 0);
+    if (answer == INT_MAX)   cout << -1 << "\n";
+    else cout << answer << "\n";
+}
+
+void input() {
+    cin >> n >> m >> h;
+
+    int a, b;
+    while (m--) {
+        cin >> a >> b;
+        if (add_ladder(a, b)) ladder[a][b] = true;
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    input();
+    solve();
+
+    return 0;
+}
+
+#else
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -71,3 +152,5 @@ int main()
 
     return 0;
 }
+
+#endif
