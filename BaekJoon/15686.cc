6ad46408be22ce5f9b2dbb66c460893db306778b
JungYoonSung <ybook2012@gmail.com>
2020-03-19 16:28:56 +0900

    15686 치킨배달
    [DFS]
    
    주의 : dfs시 넘기는 인자 확인하기
    
    답은 똑같이 나오나 TLE가 나옴.
    i 를 넘겨야 했는데,  store_n 를 넘기고 있었음. (처음부터 다 탐색 시작 해서 TLE)
    
    ---
    
    집, 치킨집 좌표를 따로 저장해두고
    치킨집을 운영 시켜보며 치킨 거리를 재본다.
    
    이때 최대 M개의 치킨집이 운영이 가능 할 때
    M개를 운영 하는 것이 항상 치킨 거리가 제일 짧다.
    
    두 가지 방법으로 풀었는데
    
    1. 치킨집 구조체마다 의 운영 상태를 좌표와 같이 관리하고, 전체 치킨집을 확인하며 운영중인 치킨집과 비교하기
    
    2. 운영 중인 치킨집을 관리하는 벡터를 따로 만들고, 
    운영중인 치킨집은 해당 벡터에 넣고
    그 벡터만 확인하기

---
+++ BaekJoon/15686.cc
@@ -0,0 +1,162 @@
+#if 01
+
+#include <bits/stdc++.h>
+#define pp pair<int,int>
+
+using namespace std;
+
+int n, m;
+int house_cnt, store_cnt;
+int answer = INT_MAX;
+
+struct Store {
+    int x, y;
+    bool run;
+};
+
+vector<pp> house;
+vector<Store> store;
+
+int calculate(const pp& h, const Store& s) {
+    return abs(h.first - s.x) + abs(h.second - s.y);
+}
+
+int get_chicken_distance() {
+    int total_distance = 0;
+    for (int i = 0; i < house_cnt; ++i) {
+        int distance = 123123;
+        for (int ii = 0; ii < store_cnt; ++ii) {
+            if (store[ii].run == false) continue;
+            distance = min(distance, calculate(house[i], store[ii]));
+        }
+        total_distance += distance;
+    }
+    return total_distance;
+}
+
+
+void dfs(int store_n, int run_cnt) {
+    if (store_cnt - store_n + run_cnt + 1 < m) return;
+    if (run_cnt == m) {
+        answer = min(answer, get_chicken_distance());
+        return;
+    }
+
+    for (int i = store_n; i < store_cnt; ++i) {
+        store[i].run = true;
+        dfs(i + 1, run_cnt + 1);
+        store[i].run = false;
+    }
+}
+
+void solve() {
+    dfs(0, 0);
+    cout << answer << "\n";
+}
+
+void input() {
+    cin >> n >> m;
+    int spot;
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii) {
+            cin >> spot;
+            if (spot == 1)   house.push_back({ i,ii });
+            else if (spot == 2) store.push_back({ i,ii, false });
+        }
+    }
+
+    house_cnt = house.size();
+    store_cnt = store.size();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    input();
+    solve();
+
+    return 0;
+}
+
+#else
+
+#include <bits/stdc++.h>
+#define pp pair<int,int>
+
+using namespace std;
+
+int n, m;
+int house_cnt, store_cnt;
+int answer = INT_MAX;
+
+vector<pp> house;
+vector<pp> store, run_store;
+
+int calculate(const pp &h, const pp &s) {
+    return abs(h.first - s.first) + abs(h.second - s.second);
+}
+
+int get_chicken_distance() {
+    int total_distance = 0;
+    for (int i = 0; i < house_cnt; ++i) {
+        int distance = 123123;
+        for (int ii = 0; ii < m; ++ii) {
+            distance = min(distance, calculate(house[i], run_store[ii]));
+        }
+        total_distance += distance;
+    }
+    return total_distance;
+}
+
+
+void dfs(int store_n, int run_cnt) {
+    if (store_cnt - store_n + run_cnt + 1 < m) return;
+    if (run_cnt == m) {
+        answer = min(answer, get_chicken_distance());
+        return;
+    }
+
+    for (int i = store_n; i < store_cnt; ++i) {
+        run_store.push_back(store[i]);
+        dfs(i + 1, run_cnt + 1);
+        run_store.pop_back();
+    }
+}
+
+
+void solve() {
+    dfs(0, 0);
+    cout << answer << "\n";
+}
+
+void input() {
+    cin >> n >> m;
+    int spot;
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii) {
+            cin >> spot;
+            if (spot == 1)   house.push_back({ i,ii });
+            else if (spot == 2) store.push_back({ i,ii });
+        }
+    }
+
+    house_cnt = house.size();
+    store_cnt = store.size();
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+
+    input();
+    solve();
+
+    return 0;
+}
+
+#endif
