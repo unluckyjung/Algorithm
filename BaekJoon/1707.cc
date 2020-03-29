379dea52bebf9a7a2035e0f849f748b83c42e08c
JungYoonSung <ybook2012@gmail.com>
2020-03-30 00:15:55 +0900

    1707 이분 그래프
    [그래프 DFS]
    
    이분 그래프를 어떻게 확인 할 수 있을지에 대한 아이디어를 떠올리는데 시간을 꽤 소비했다.
    
    연결 되어있는 노드 중 마킹이 되어있지 않은 노드에 현재 노드와 다른 마킹을 한다. 
    
    모든 마킹이 끝낸 후, 각 노드별로 그래프를 순회하는데 
    모든 노드중 되어있는 것 들 중 같은 것으로 마킹 되어있지 않으면 이분 그래프가 된다.

---
+++ BaekJoon/1707.cc
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int t;
+vector<int> node[20002];
+int check[20002];
+int V, E;
+
+bool answer_check() {
+    for (int i = 1; i <= V; ++i) {
+        for (auto connect : node[i]) {
+            if (check[i] == check[connect]) return false;
+        }
+    }
+    return true;
+}
+
+void dfs(int cur) {
+    for (auto next : node[cur]) {
+        if (check[next] != 0) continue;
+        check[next] = check[cur] * -1;
+        dfs(next);
+    }
+}
+
+void solve() {  //TODO : 접근했는데, 이미 마킹이 되어있고, 같은거라면 가지치기 기능.
+    for (int i = 1; i <= V; ++i) {
+        if (check[i] != 0)continue;
+        check[i] = 1;   // mistake 1 :마킹을 안하고 dfs를 해서 무한루프
+        dfs(i);
+    }
+    if (answer_check()) cout << "YES\n";
+    else cout << "NO\n";
+}
+
+void init() {
+    for (int i = 1; i <= V; ++i) {
+        node[i].clear();
+        check[i] = 0;
+    }
+
+    int a, b;
+    while (E--) {
+        cin >> a >> b;
+        node[a].push_back(b);
+        node[b].push_back(a);
+    }
+}
+
+void input() {
+    cin >> t;
+    while (t--) {
+        cin >> V >> E;
+        init();
+        solve();
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+
+    input();
+
+    return 0;
+}
