da4c39d11d91824eac9151fe3b644923ad609e96
JungYoonSung <ybook2012@gmail.com>
2020-04-06 14:46:20 +0900

    fix : #백준 3009 네 번째 점 재 채점으로 인한 풀이
    xor 연산을 이용하여 등장하지 않은 꼭지점을 찾아냄.

--- BaekJoon/3009.cc
+++ BaekJoon/3009.cc
@@ -1,39 +1,32 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int arr1[3];
-int arr2[3];
+int a1, a2, a3, b1, b2, b3;
 
-int arr1_2[1000];
-int arr2_2[1000];
+void solve() {
+    int a = a1 ^ a2;
+    a = a ^ a3;
+
+    int b = b1 ^ b2;
+    b = b ^ b3;
+
+    cout << a << " " << b << "\n";
+}
+
+void input() {
+    cin >> a1 >> b1;
+    cin >> a2 >> b2;
+    cin >> a3 >> b3;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
 
-    for (int i = 0; i < 3; i++)
-    {
-        cin >> arr1[i] >> arr2[i];
-        arr1_2[arr1[i]]++;
-        arr2_2[arr2[i]]++;
-    }
+    input();
+    solve();
 
-    for (int i = 0; i < 3; i++)
-    {
-        if (arr1_2[arr1[i]] != 2)
-        {
-            cout << arr1[i] << " ";
-        }
-    }
-
-    for (int i = 0; i < 3; i++)
-    {
-        if (arr2_2[arr2[i]] != 2)
-        {
-            cout << arr2[i];
-        }
-    }
     return 0;
 }
