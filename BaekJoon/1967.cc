4409465f6e733d9eb8308f6df26c766020f3072e
JungYoonSung <ybook2012@gmail.com>
2020-04-16 14:52:57 +0900

    add : #백준 1967 트리의 지름
    [BFS]
    
    문제 해석 : 가장 먼 정점의 거리를 찾는다.
    
    문제 재 해석 : 루트 노드로부터 가장 먼 정점을 찾는다.
    찾은 정점으로부터 가장 먼 정점을 찾는다.
    
    설계 : BFS 를 통해 가장 먼 정점과, 거리를 찾는다.
    
    피드백 : 문제를 잘 보자. 이 문제는 양방향 간선을 한번에 다 넣었어야 한다.

---
+++ BaekJoon/1967.cc
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h>
+#define pp pair<int, int>
+#define MAX 100002
+
+using namespace std;
+
+vector<pp> node[MAX];
+int dist[MAX];
+bool visit[MAX];
+
+int n;
+
+queue<int> q;
+
+int bfs(int cmd){
+    int ret_dist = 0;
+    int ret_node = 0;
+
+    while(!q.empty()){
+        int cur_node = q.front(); q.pop();
+
+        for(auto nxt : node[cur_node]){
+            int nxt_node = nxt.first;
+            int nxt_weight = nxt.second;
+
+            if(visit[nxt_node]) continue;
+            visit[nxt_node] = true;
+            dist[nxt_node] = dist[cur_node] + nxt_weight;
+
+            if(ret_dist < dist[nxt_node]){
+                ret_dist = dist[nxt_node];
+                ret_node = nxt_node;
+            }
+            q.push(nxt_node);
+        }
+    }
+
+    if(cmd) return ret_node;
+    else return ret_dist;
+}
+
+void solve() {
+    const int root = 1;
+
+    visit[root] = true;
+    q.push(root);
+    int start = bfs(1);
+
+    memset(dist, 0 , sizeof(dist));
+    memset(visit, false, sizeof(visit));
+
+    visit[start] = true;
+    q.push(start);
+
+    cout << bfs(0);
+}
+
+void input() {
+    cin >> n;
+    for(int i = 0 ; i< n ; ++i){
+        int a, b, weight;
+        cin >> a >> b >> weight;
+
+        node[a].push_back({b, weight});
+        node[b].push_back({a, weight});
+    }
+}
+
+int main()
+{
+   ios_base::sync_with_stdio(false);
+   cin.tie(NULL);  cout.tie(NULL);
+   freopen("input.txt", "r", stdin);
+
+   input();
+   solve();
+
+   return 0;
+}
