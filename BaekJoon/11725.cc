825f11bd44eb7ee8d5589727d17ffb1d022c8ce4
JungYoonSung <ybook2012@gmail.com>
2020-04-16 15:59:54 +0900

    update : #백준 11725 트리의 부모찾기
    접근 하려는 것이 부모인 경우를 판별한다.

--- BaekJoon/11725.cc
+++ BaekJoon/11725.cc
@@ -8,16 +8,32 @@ bool visit[100002];
 int n;
 const int root = 1;
 
+#if 0
+
+//  방문 하려는것이 이전에 방문한 노드인 경우.
 void dfs(int cur){
     if(visit[cur]) return;
     visit[cur] = true;
     
     for(int nxt : node[cur]){
-        if(nxt == cur)  continue;
         if(p[nxt] == 0) p[nxt] = cur;
         dfs(nxt);
     }
 }
+
+#else if
+
+//  방문 하려는것이 부모인 경우
+void dfs(int cur) {
+
+    for (int nxt : node[cur]) {
+        if (nxt == p[cur])  continue;
+        if (p[nxt] == 0) p[nxt] = cur;
+        dfs(nxt);
+    }
+}
+
+#endif
 
 
 void solve() {
