3f5794003921d4e9fbbbe5098f7a36aa3e9cc7d5
JungYoonSung <ybook2012@gmail.com>
2020-04-17 21:48:59 +0900

    update : #백준 1629 곱셈
    [분할 정복]
    
    ## 문제 해석
    * a^b % c를 구하라
    
    ## 문제 재해석
    * 범위가 21억이다.
    * log(N) 이하의 풀이를 찾아야 한다.
    * 이전 정보를 이용한다.
    
    ## 해결 전략
    * N ^ 10은( N ^ 5  ) ^ 2로 나타낼 수 있다.
    * N ^ 11은( N ^ 5 ) ^ 2 * N로 나타낼 수 있다.
    * 분할 정복으로 해결한다.
    * a ^ b 를 (a ^ (b / 2) ) ^ 2 +? b로 구한다.
    
    
    ## 설계 , 구현
    * b가 0일때는 a가 무엇이던 항상 1이다.
    * b가 홀수라면 계산한 값에서 a를 한번 더 곱해준다.
    
    ## 주의할 점
    * long long을 사용해야 한다.
    * 추가 연산이 진행된 뒤에는 항상 mod 연산을 해준다.

--- BaekJoon/1629.cc
+++ BaekJoon/1629.cc
@@ -1,32 +1,40 @@
 #include <bits/stdc++.h>
-using namespace std;
-
-//1629 곱셈.
-
 #define ll long long
 
-ll ans(ll a, ll n, ll m) {
-	if (n == 0) return 1;
-	//0승인경우 1리턴.
-	ll val = ans(a, n / 2, m);
-	//2^10이 들어왔을때 2^5를 만든다.
-	val = val * val % m;
-	//2^5 * 2^5 = 2^10 이다.
-	if (n & 1)	return val* a% m;
-	//만약 홀수 2^11 인경우, 2^5 * 2^5 한것에 2를 한번더 곱한다.
-	return val;
-	//아닌경우 받아온 결과값 리턴.
+using namespace std;
+
+ll a, b, c;
+
+ll _pow(ll base, ll expo, ll mod) {
+    if (expo == 0) return 1;
+    
+    ll ret = _pow(base, expo / 2, mod);
+
+    ret = (ret * ret) % mod;
+
+    if (expo % 2 != 0) {
+        ret *= (base % mod);
+        ret %= mod;
+    }
+    return ret;
+}
+
+void solve() {
+    cout << _pow(a, b, c);
+}
+
+void input() {
+    cin >> a >> b >> c;
 }
 
 int main()
 {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
 
-	ll a, n, m;
-	cin >> a >> n >> m;
-	cout << ans(a, n, m);
+    input();
+    solve();
 
-	return 0;
+    return 0;
 }
