6e0d269b0cad37cb8bc65e17b4091dc18ab78e0a
JungYoonSung <ybook2012@gmail.com>
2020-04-20 16:26:07 +0900

    update : #백준 1182 부분수열의 합
    [완전 탐색, 백트래킹]
    
    ## 문제 해석
    * 부분 수열 중 합이 S가 되는 경우의 수를 구하라.
    
    ## 문제 재해석
    * 수열 중 임의의 숫자를 뽑고 그것이 합이 S가 되는지 구하라.
    
    ## 해결 전략
    * 더하거나 더하지 않거나, 하는 방식으로 재귀를 진행한다.
    
    ## 구현
    * 존재하지 않는 숫자에 접근하면 가지치기를 한다.
    * 해당 숫자를 더해보고,  결과가 S라면 경우의 수를 증가 시킨다.
    
    ## 주의할 점
    * 답이 0인경인 경우를 조심해야 한다.
    
    ## 피드백
    * 예제가 예외를 던져줘서 한번에 풀 수 있었다.

--- BaekJoon/1182.cc
+++ BaekJoon/1182.cc
@@ -1,26 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
+
 int n, s;
-int arr[21];
-int answer;
+int cnt;
+
+int arr[22];
+
+void dfs(int cur, int idx) {
+
+	if (idx == n) return;
+	if (cur + arr[idx] == s)  cnt++;
+
+	dfs(cur + arr[idx], idx + 1);
+    dfs(cur, idx + 1);
+}
+
+void solve() {
+    dfs(0, 0);
+    cout << cnt << "\n";
+}
+
+void input() {
+    cin >> n >> s;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+}
 
 int main()
 {
     ios_base::sync_with_stdio(false);
-    /*제출전 주석처리*/freopen("input.txt", "r", stdin);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin >> n >> s;
-    for(int i = 0; i<n; ++i)    cin >> arr[i];
+    cin.tie(NULL);  cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
 
-    for(int i = 1; i < (1 << n); ++i){
-        int sum = 0;
-        for(int index = 0; index < n; ++index){
-            if(i&(1 << index)) sum+=arr[index];
-        }
-        if(sum == s)answer ++;
-    }
-    cout << answer <<"\n";
+    input();
+    solve();
+
     return 0;
 }
