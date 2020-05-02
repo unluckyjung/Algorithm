c3f9f4a46fbd10958d1482c0136fecd085babefe
JungYoonSung <ybook2012@gmail.com>
2020-05-02 14:49:08 +0900

    add : #LeetCode Day1 First Bad Version
    ## 문제 해석
    * 불량품이 생기기 시작하는 첫 번째 버전을 찾아라.
    
    ## 문제 재해석
    * 불량품을 만드는 가장 작은 버전을 찾아라
    * 불량품이면 true를 리턴 하는 API를 최소한으로 호출하라.
    * 탐색 문제이다.
    
    ## 해결 전략
    * 문제에서 불량품은 항상 존재한다.
    * 제품의 버전은 1번부터 N번 까지 있다.
    * 정렬되어 있으므로, 이분 탐색을 통해 불량품을 찾는다.
    
    ## 설계, 구현
    * mid가 불량품인 경우, 해당 버전을 끝으로 하고 재 탐색한다.
    * mid가 불량품이 아닌 경우, 다음 버전을 시작으로 하고 재 탐색한다.
    * `s + e` 연산에서 일어 날 수 있는 오버 플로를 방지하기 위해 
        * `(s + e) / 2` 대신 `s + (e - s) / 2` 로 mid 값을 찾았다.
    
    ## 주의할 점
    * Overflow를 조심하라.
    
    ## 디버깅
    * mid가 불량품인 경우. 불량품인 mid를 포함해서 다시 재 탐색을 해야 하는데, 일반적인 원하는 수를 찾는 `e = mid -1` 로 잘못 구현하여 잘못된 값이 나왔었다.
    * s + e를 하여 오버플로가 발생했었다.
    
    
    ## 회고
    
    * 문제에서 찾는 것이 무엇 인지를 명확히 하자.
    * 매번 오버플로를 방지하기 위해 long long을 이용했었는데, 새로운것을 배웠다.

---
+++ LeetCode/May_Challenge/Day1_First Bad Version.cc
@@ -0,0 +1,22 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int s = 1, e = n;
+        int mid;
+        
+        while(s <= e){       
+            if(s == e) return s;
+
+            mid = s + (e - s) / 2;              
+
+            if(isBadVersion(mid)) e = mid;
+            else s = mid + 1;
+        }      
+        
+
+        return -1;
+    }
+};
