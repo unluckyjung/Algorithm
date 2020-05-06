0c87c3acf4b26c2e2718e92b11cbcd60fe639236
JungYoonSung <ybook2012@gmail.com>
2020-05-06 19:55:41 +0900

    update : #백준 16234 인구 이동 속도 개선
    ## 이전 소스
    * 연합을 이루는 나라들의 좌표를 기억해두고, 큐에서 빼면서 평균값을 대입 시켜 주었다.
    
    ## 개선 내용
    * 연합을 이루는 나라들의 좌표 리스트를 만들어둔다.
    * 연합을 이루는 나라들에게 평균값을 대입 시켜준다.
    * 즉, 큐에서 빼는 작업이 생략되어 시간이 개선되었다.
    
    ## 주의할 점
    * 한턴에 여러개의 연합이 만들어졌어도, 인구 이동은 한번만 일어난 것으로 한다.
    * 연합을 다 만들어보고, 인구 이동을 진행하고, 연합을 전부 해체하는 순서 이기 때문이다.
    
    ## 디버깅
    * 연합 리스트에 넣는 작업을 큐에서 빼는 순간만 해야 하는데, 실수로 BFS 큐에 넣을때 같이 연합 리스트에 중복 해서 넣는 실수를 했다.

--- BaekJoon/16234.cc
+++ BaekJoon/16234.cc
@@ -1,3 +1,108 @@
+#if 01
+
+#include <bits/stdc++.h>
+#define pp pair<int,int>
+#define x first
+#define y second
+
+using namespace std;
+
+int n, L, R;
+int board[52][52];
+bool visit[52][52];
+
+const int dx[] = { -1,1,0,0 };
+const int dy[] = { 0,0,-1,1 };
+
+queue<pp> q;
+
+bool Is_Moved() {
+
+    vector<pp> team;
+    int team_sum = 0;
+
+    while (!q.empty()) {
+        pp cur = q.front(); q.pop();
+
+        int cur_people = board[cur.x][cur.y];
+        team_sum += cur_people;
+        team.push_back(cur);
+
+
+        for (int dir = 0; dir < 4; ++dir) {
+            int nx = cur.x + dx[dir];
+            int ny = cur.y + dy[dir];
+
+            if (nx < 0 or nx >= n or ny < 0 or ny >= n) continue;
+            if (visit[nx][ny]) continue;
+
+            int nxt_people = board[nx][ny];
+            int diff = abs(cur_people - nxt_people);
+
+            if (diff < L or diff > R) continue;
+            visit[nx][ny] = true;
+            q.push({ nx,ny });
+        }
+    }
+
+    int team_cnt = team.size();
+    if (team_cnt == 1) return false;
+
+    int avg = team_sum / team_cnt;
+
+    for (pp country : team) {
+        board[country.x][country.y] = avg;
+    }
+    return true;
+}
+
+void Solve() {
+    int answer = 0;
+
+    while (1) {
+        bool is_moved = false;
+        memset(visit, false, sizeof(visit));
+
+        for (int i = 0; i < n; ++i) {
+            for (int ii = 0; ii < n; ++ii) {
+                if (visit[i][ii]) continue;
+
+                visit[i][ii] = true;
+                q.push({ i,ii });
+                if (Is_Moved() and !is_moved) is_moved = true;
+            }
+        }
+
+        if (is_moved) answer++;
+        else break;
+    }
+    cout << answer << "\n";
+}
+
+void Input() {
+    cin >> n >> L >> R;
+
+    for (int i = 0; i < n; ++i) {
+        for (int ii = 0; ii < n; ++ii) {
+            cin >> board[i][ii];
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);  cout.tie(NULL);
+    //freopen("input.txt", "r", stdin);
+
+    Input();
+    Solve();
+
+    return 0;
+}
+
+#else
+
 #include <bits/stdc++.h>
 #define pp pair<int,int>
 #define x first
@@ -106,3 +211,5 @@ int main()
 
     return 0;
 }
+
+#endif
