37c8c3be9937afccf502ed733b8a8565bd805fc2
JungYoonSung <ybook2012@gmail.com>
2020-07-20 23:57:01 +0900

    add : #백준 16987 계란으로 계란치기
    * 아직 풀이중인 코드.
    * 예제 6번에 대해서 오답이 나온다.

---
+++ BaekJoon/16987.cc
@@ -0,0 +1,77 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct egg {
+    int durability;
+    int weight;
+};
+
+int n, answer;
+egg eggs[10];
+bool isUsed[10];
+
+int HitEgg(int attackEgg, int defenceEgg) {
+
+    int brokenCnt = 0;
+    isUsed[defenceEgg] = true;
+
+    eggs[attackEgg].durability -= eggs[defenceEgg].weight;
+    eggs[defenceEgg].durability -= eggs[attackEgg].weight;
+
+    if (eggs[attackEgg].durability <= 0) brokenCnt++;
+    if (eggs[defenceEgg].durability <= 0) brokenCnt++;
+
+    return brokenCnt;
+}
+
+void RecoveryEgg(int attackEgg, int defenceEgg) {
+    eggs[attackEgg].durability += eggs[defenceEgg].weight;
+    eggs[defenceEgg].durability += eggs[attackEgg].weight;
+    isUsed[defenceEgg] = false;
+}
+
+void SelectEgg(int attackEgg, int brokenCnt) {
+
+    if (attackEgg == n) {
+        answer = max(answer, brokenCnt);
+        return;
+    }
+
+    if (eggs[attackEgg].durability <= 0) {
+        SelectEgg(attackEgg + 1, brokenCnt);
+        return;
+    }
+
+    for (int defenceEgg = 0; defenceEgg < n; ++defenceEgg) {
+        if (defenceEgg == attackEgg) continue;
+        if (isUsed[defenceEgg]) continue;
+        if (eggs[defenceEgg].durability <= 0) continue;
+
+        SelectEgg(attackEgg + 1, brokenCnt + HitEgg(attackEgg, defenceEgg));
+        RecoveryEgg(attackEgg, defenceEgg);
+    }
+}
+
+void Solve() {
+    SelectEgg(0, 0);
+    cout << answer << "\n";
+}
+
+void Input() {
+    cin >> n;
+
+    for (int i = 0; i < n; ++i) {
+        cin >> eggs[i].durability >> eggs[i].weight;
+    }
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);   cout.tie(NULL);
+
+    freopen("input.txt", "r", stdin);
+    Input();
+    Solve();
+
+    return 0;
+}
