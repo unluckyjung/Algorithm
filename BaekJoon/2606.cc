52dca2b5e6cda177d72b0c2c22f6592967d5a737
yoonsung.jung <ybook2012@gmail.com>
2021-04-24 21:51:09 +0900

    fix : 백준 컴파일러 버전 변경으로 인한 런타임 에러 해결

--- BaekJoon/2606.cc
+++ BaekJoon/2606.cc
@@ -1,47 +1,47 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 vector<int> node[102];
-bool visit[102];
+bool isVisited[102];
 
 int computer_n, m;
 int answer;
 
-void dfs(int cur){
-    for(auto next : node[cur]){
-        if(visit[next])continue;
-        visit[next] = true;
+void dfs(int cur) {
+    for (auto next : node[cur]) {
+        if (isVisited[next]) continue;
+        isVisited[next] = true;
 
         answer++;
         dfs(next);
     }
 }
 
-void solve(){
-    visit[1] = true;
+void solve() {
+    isVisited[1] = true;
     dfs(1);
     cout << answer << "\n";
 }
 
-void input(){
+void input() {
     cin >> computer_n >> m;
 
     int a, b;
-    while(m--){
+    while (m--) {
         cin >> a >> b;
         node[a].push_back(b);
         node[b].push_back(a);
     }
 }
 
-int main()
-{
-   ios_base::sync_with_stdio(false);
-   cin.tie(NULL);  cout.tie(NULL);
-   freopen("input.txt", "r", stdin);
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr); cout.tie(nullptr);
+    freopen("input.txt", "r", stdin);
 
-   input();
-   solve();
+    input();
+    solve();
 
-   return 0;
+    return 0;
 }
