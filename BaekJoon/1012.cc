434c23156687c8d98d395e0a76df3ca1c8f7247c
unluckyjung <ybook2012@gmail.com>
2021-08-17 15:41:14 +0900

    fix(1012): 백준 컴파일러 변경으로 인해 오답처리된 문제 재풀이

--- BaekJoon/1012.cc
+++ BaekJoon/1012.cc
@@ -1,60 +1,67 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
-//1012 유기농 배추
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
+
 int board[52][52];
-bool visit[52][52];
+bool visited[52][52];
 
 int n, m;
 int k;
-int x, y;
 
+void bfs(int &num, const int &i, const int &ii);
 
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  freopen("input.txt", "r", stdin);
 
-int main()
-{
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+  int T;
+  cin >> T;
+  while (T--) {
+    memset(board, 0, sizeof(board));
+    memset(visited, false, sizeof(visited));
 
-	int T;
-	cin >> T;
-	while (T--) {
-		//초기화를 해줘야한다.
-		memset(board, 0, sizeof(board));
-		memset(visit, false, sizeof(visit));
+    cin >> n >> m >> k;
+    while (k--) {
+      int x, y;
+      cin >> x >> y;
+      board[x][y] = 1;
+    }
 
-		cin >> n >> m >> k;
-		while (k--) {
-			cin >> x >> y;
-			board[x][y] = 1;
-		}
-		int num = 0;
+    int num = 0;
+    for (int i = 0; i < n; ++i) {
+      for (int ii = 0; ii < m; ++ii) {
+        if (visited[i][ii] or board[i][ii] == 0) continue;
+        bfs(num, i, ii);
+      }
+    }
+    cout << num << "\n";
+  }
 
-		for (int i = 0; i < n; ++i) {
-			for (int ii = 0; ii < m; ++ii) {
-				if (visit[i][ii] or board[i][ii] == 0) continue;
-				queue<pair<int, int>> q;
-				visit[i][ii] = true; q.push({ i,ii });
-				num++;
+  return 0;
+}
 
-				while (!q.empty()) {
-					auto cur = q.front(); q.pop();
+void bfs(int &num, const int &i, const int &ii) {
+  queue<pair<int, int>> q;
+  visited[i][ii] = true;
+  q.push({i, ii});
+  num++;
 
-					for (int dir = 0; dir < 4; ++dir) {
-						int nx = cur.first + dx[dir];
-						int ny = cur.second + dy[dir];
-						if (visit[nx][ny] or board[nx][ny] == 0) continue;
-						if (nx < 0 or nx >= n or ny < 0 or ny >= m) continue;
-						visit[nx][ny] = true; q.push({ nx,ny });
-					}
-				}
-			}
-		}
-		cout << num << "\n";
-	}
+  while (!q.empty()) {
+    auto cur = q.front();
+    q.pop();
 
-	return 0;
+    for (int dir = 0; dir < 4; ++dir) {
+      int nx = cur.first + dx[dir];
+      int ny = cur.second + dy[dir];
+
+      if (visited[nx][ny] or board[nx][ny] == 0) continue;
+      if (nx < 0 or nx >= n or ny < 0 or ny >= m) continue;
+      visited[nx][ny] = true;
+      q.push({nx, ny});
+    }
+  }
 }
