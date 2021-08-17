e25759b8848e6d56604fa51e62a65948fcf90a53
unluckyjung <ybook2012@gmail.com>
2021-08-17 15:44:36 +0900

    fix(1929): 백준 컴파일러 변경으로 인해 오답처리된 문제 재풀이

--- BaekJoon/1929.cc
+++ BaekJoon/1929.cc
@@ -1,39 +1,40 @@
 #include <bits/stdc++.h>
-#define MAX 1000001
+
 using namespace std;
 
-int visit[MAX];
+const int MAX = 1000001;
+
+int visited[MAX];
 int n, m;
 
-
-void prime_check(int s, int e) {
-    for (int i = s; i <= e; ++i) {
-        if (visit[i] == 1) {
-            cout << i << "\n";
-        }
+void primeCheck(int s, int e) {
+  for (int i = s; i <= e; ++i) {
+    if (visited[i] == 1) {
+      cout << i << "\n";
     }
+  }
 }
 
 void solve() {
-    for (int i = 2; i <= m; ++i) {
-        for (int ii = i; ii <= m; ii += i) {
-            visit[ii]++;
-        }
+  for (int i = 2; i <= m; ++i) {
+    for (int ii = i; ii <= m; ii += i) {
+      visited[ii]++;
     }
-    prime_check(n, m);
+  }
+  primeCheck(n, m);
 }
 
 void input() {
-    cin >> n >> m;
+  cin >> n >> m;
 }
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    freopen("input.txt", "r", stdin);
-    cin.tie(NULL);  cout.tie(NULL);
 
-    input();
-    solve();
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+//  freopen("input.txt", "r", stdin);
 
-    return 0;
+  input();
+  solve();
+
+  return 0;
 }
