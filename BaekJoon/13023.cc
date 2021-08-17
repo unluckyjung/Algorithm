904a64810eaf35d60818b219d882f5046dbfbbc8
unluckyjung <ybook2012@gmail.com>
2021-08-17 15:49:33 +0900

    fix(13023): 백준 컴파일러 변경으로 인해 오답처리된 문제 재풀이

--- BaekJoon/13023.cc
+++ BaekJoon/13023.cc
@@ -1,58 +1,57 @@
 #include <bits/stdc++.h>
+
 using namespace std;
 
 vector<int> human[2002];
-bool visit[2002];
+bool visited[2002];
 int n, m;
-bool find_answer;
+bool findAnswer;
 
 void dfs(int cur, int depth) {
-    if (find_answer) return;
+  if (findAnswer) return;
 
-    if (depth == 4) {
-        find_answer = true;
-        return;
-    }
+  if (depth == 4) {
+    findAnswer = true;
+    return;
+  }
 
-    for (auto nxt : human[cur]) {
-        if (visit[nxt])continue;
-        visit[nxt] = true;
-        dfs(nxt, depth + 1);
-        visit[nxt] = false;
+  for (auto nxt : human[cur]) {
+    if (visited[nxt])continue;
+    visited[nxt] = true;
+    dfs(nxt, depth + 1);
+    visited[nxt] = false;
 
-        if (find_answer) return;
-    }
-  
+    if (findAnswer) return;
+  }
+
 }
 
 int solve() {
-    for (int i = 0; i < n; ++i) {
-        visit[i] = true;
-        dfs(i, 0);
-        visit[i] = false;
-        if (find_answer) return 1;
-    }
-    return 0;
+  for (int i = 0; i < n; ++i) {
+    visited[i] = true;
+    dfs(i, 0);
+    visited[i] = false;
+    if (findAnswer) return 1;
+  }
+  return 0;
 }
 
 void input() {
-    cin >> n >> m;
-    int a, b;
-    while (m--) {
-        cin >> a >> b;
-        human[a].push_back(b);
-        human[b].push_back(a);
-    }
+  cin >> n >> m;
+  int a, b;
+  while (m--) {
+    cin >> a >> b;
+    human[a].push_back(b);
+    human[b].push_back(a);
+  }
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);  cout.tie(NULL);
-    freopen("input.txt", "r", stdin);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
 
-    input();
-    cout << solve() << "\n";
+  input();
+  cout << solve() << "\n";
 
-    return 0;
+  return 0;
 }
