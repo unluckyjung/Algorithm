f1c32165cc181204b24ad0e37ff6796316dc5ddf
unluckyjung <ybook2012@gmail.com>
2021-09-27 01:24:10 +0900

    fix(백준 소문난 칠공주): cpp 풀이 리팩토링

--- BaekJoon/1941.cc
+++ BaekJoon/1941.cc
@@ -1,95 +1,99 @@
 #include <bits/stdc++.h>
-#define pp pair<int,int>
+
+#define pii pair<int,int>
 using namespace std;
 
+const int dy[] = {0, 0, -1, 1};
+const int dx[] = {-1, 1, 0, 0};
+
 char board[5][5];
+bool canGo[5][5];
+bool visited[5][5];
 
-bool visit[5][5];
-bool can_go[5][5];
-vector<int> vec;
+vector<int> member(25 - 7, 0);
 
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
+queue<pii > location;
 
 int n = 5;
-int answer;
 
-
-void bfs(int sx, int sy) {
-    queue<pp> q;
-    visit[sx][sy] = true;
-    q.push({ sx,sy });
-    int move_cnt = 1;
-
-    while (!q.empty()) {
-        pp cur = q.front(); q.pop();
-        for (int dir = 0; dir < 4; ++dir) {
-            int nx = cur.first + dx[dir];
-            int ny = cur.second + dy[dir];
-
-            if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
-            if (!can_go[nx][ny])continue;
-            if (visit[nx][ny])continue;
-
-            move_cnt++;
-            visit[nx][ny] = true;
-            q.push({ nx,ny });
-        }
-    }
-    if (move_cnt == 7)answer++;
+void init() {
+  memset(canGo, false, sizeof(canGo));
+  memset(visited, false, sizeof(visited));
+  location = queue<pii >();
 }
 
-void solve() {
-    for (int i = 0; i < 25; ++i) {
-        if (i < 25 - 7)vec.push_back(0);
-        else vec.push_back(1);
+bool locationCheck() {
+  int memberCount = 0;
+
+  while (!location.empty()) {
+    pii cur = location.front();
+    location.pop();
+    memberCount++;
+
+    for (int dir = 0; dir < 4; ++dir) {
+      int nx = cur.first + dx[dir];
+      int ny = cur.second + dy[dir];
+
+      if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
+      if (visited[nx][ny])continue;
+      if (!canGo[nx][ny])continue;
+
+      visited[nx][ny] = true;
+      location.push({nx, ny});
     }
+  }
 
-    do {
-        int y_count = 0;
-        memset(can_go, false, sizeof(can_go));
-        memset(visit, false, sizeof(visit));
+  return memberCount == 7;
+}
 
-        int start_x, start_y;
-        for (int i = 0; i < 25; ++i) {
-            if (vec[i] != 1)continue;
+bool teamCheck() {
+  int Y_count = 0;
 
-            int x = i / n;
-            int y = i % n;
+  int x, y;
+  for (int i = 0; i < 25; ++i) {
+    if (member[i] != 1)continue;
+    x = i / n;
+    y = i % n;
+    if (board[x][y] == 'Y') Y_count++;
 
-            if (board[x][y] == 'Y') y_count++;
+    canGo[x][y] = true;
+    if (Y_count >= 4) return false;
+  }
 
-            can_go[x][y] = true;
-            start_x = x;
-            start_y = y;
-        }
-        if (y_count >= 4) continue;
-        
-        bfs(start_x, start_y);
+  visited[x][y] = true;
+  location.push({x, y});
+  return locationCheck();
+}
 
-    } while (next_permutation(vec.begin(), vec.end()));
+int solve() {
+  int answer = 0;
 
-    cout << answer << "\n";
+  do {
+    init();
+    if (teamCheck()) answer++;
+  } while (next_permutation(member.begin(), member.end()));
+
+  return answer;
 }
 
 void input() {
-    string line;
-    for (int i = 0; i < n; ++i) {
-        cin >> line;
-        for (int ii = 0; ii < n; ++ii) {
-            board[i][ii] = line[ii];
-        }
+  string line;
+  for (int i = 0; i < n; ++i) {
+    cin >> line;
+    for (int ii = 0; ii < n; ++ii) {
+      board[i][ii] = line[ii];
     }
+  }
+  for (int i = 0; i < 7; ++i) member.push_back(1);
 }
 
-int main()
-{
-    ios_base::sync_with_stdio(false);
-    freopen("input.txt", "r", stdin);
-    cin.tie(NULL);  cout.tie(NULL);
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+  freopen("input.txt", "r", stdin);
 
-    input();
-    solve();
+  input();
+  cout << solve() << "\n";
 
-    return 0;
+  return 0;
 }
