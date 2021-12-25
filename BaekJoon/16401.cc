3782ae649478f23632a5ee129d1d9585d78e701b
unluckyjung <ybook2012@gmail.com>
2021-12-26 02:32:21 +0900

    add(백준 16401 과자 나눠주기): cpp풀이
    ## 문제해석
    - 모든 사람에게 동일한 길이의 최대한 긴 과자를 나누어줄때, 과자 길이를 구하라
    
    ## 해결전략
    - 파라메트릭 서치로 해결한다.
    - 길이 x의 과자를 나누어주었을때, 전부 나누어 줄 수 있는지를 해결한다.
    
    ## 구현
    - 과자의 최대길이를 max값으로 잡고 이분탐색해 나간다.

---
+++ BaekJoon/16401.cc
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int humanCount, cookieCount;
+int cookies[1000002];
+int maxCookieSize;
+int answer;
+
+bool isCanDivide(int cookieSize) {
+  if (cookieSize == 0) return false;
+  int dividedCookieCount = 0;
+  for (int i = 0; i < cookieCount; ++i) {
+    dividedCookieCount += cookies[i] / cookieSize;
+  }
+  return dividedCookieCount >= humanCount;
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+
+  cin >> humanCount >> cookieCount;
+  for (int i = 0; i < cookieCount; ++i) {
+    cin >> cookies[i];
+    maxCookieSize = max(maxCookieSize, cookies[i]);
+  }
+
+  int st = 0;
+  int ed = maxCookieSize;
+  int mid;
+
+  while (st <= ed) {
+    mid = st + (ed - st) / 2;
+    if (isCanDivide(mid)) {
+      answer = mid;
+      st = mid + 1;
+    } else {
+      ed = mid - 1;
+    }
+  }
+
+  cout << answer << "\n";
+  return 0;
+}
