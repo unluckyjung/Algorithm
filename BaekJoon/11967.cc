77af7b47a41e4622656771f0b46076357a044092
unluckyjung <ybook2012@gmail.com>
2022-01-13 02:32:05 +0900

    update: 백준 컴파일러 변경으로 인한 풀이변경
    2년전에 푼 문제인데 로직이 굉장히  더럽다.
    나중에 자바로 다시풀때 코드를 정리해야할 문제

--- BaekJoon/11967.cc
+++ BaekJoon/11967.cc
@@ -1,101 +1,97 @@
 #include<bits/stdc++.h>
+#define pii pair<int, int>
+
 using namespace std;
 
-int board[101][101];
-bool visit[101][101];
-bool light[101][101];
-int cnt;
+bool isVisited[101][101], isLightOn[101][101];
+int answer = 1;
 
-map<pair<int, int>, queue<pair<int, int> > >light_m;
-queue<pair<int, int> > lq;
-queue<pair<int, int> > q;
+map<pii, queue<pii>> switchMap;
+queue<pii> lightQ, q;
 
-int dx[] = { -1,1,0,0 };
-int dy[] = { 0,0,-1,1 };
+const int dx[] = {-1, 1, 0, 0};
+const int dy[] = {0, 0, -1, 1};
 
 int n, m;
 
-void output() {
-	for (int i = 0; i < n; ++i) {
-		for (int ii = 0; ii < n; ++ii) {
-			cout << visit[i][ii];
-		}
-		cout << "\n";
-	}
-}
+void lightOn(pii k) {
+  while (!switchMap[k].empty()) {
+    auto cur = switchMap[k].front();
+    switchMap[k].pop();
 
-void light_on(pair<int, int> k) {
-	while (!light_m[k].empty()) {
-		auto cur = light_m[k].front();	light_m[k].pop();
-		if (!light[cur.first][cur.second]) {
-			light[cur.first][cur.second] = true;
-			lq.push({ cur.first, cur.second });
-			cnt++;
-		}
-	}
+    if (!isLightOn[cur.first][cur.second]) {
+      isLightOn[cur.first][cur.second] = true;
+      lightQ.push({cur.first, cur.second});
+      answer++;
+    }
+  }
 }
 
 void bfs() {
-	if (q.empty())return;
+  if (q.empty()) return;
 
-	while (!q.empty()) {
-		auto cur = q.front(); q.pop();
-		//light_on(cur);
+  while (!q.empty()) {
+    auto cur = q.front();
+    q.pop();
 
-		for (int dir = 0; dir < 4; ++dir) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-			if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
-			if (visit[nx][ny])continue;
-			if (!light[nx][ny])continue;
-			visit[nx][ny] = true;
-			light_on({ nx,ny });
-			q.push({ nx,ny });
-		}
-	}
-	while (!lq.empty()) {
-		auto cur = lq.front();	lq.pop();
-		if (visit[cur.first][cur.second])continue;
+    for (int dir = 0; dir < 4; ++dir) {
+      int nx = cur.first + dx[dir];
+      int ny = cur.second + dy[dir];
 
-		for (int dir = 0; dir < 4; ++dir) {
-			int nx = cur.first + dx[dir];
-			int ny = cur.second + dy[dir];
-			if (nx < 0 or nx >= n or ny < 0 or ny >= n)continue;
-			if (visit[nx][ny]) {
-				visit[cur.first][cur.second] = true;
-				light_on({ cur.first,cur.second });
-				q.push({ cur.first, cur.second });
-				break;
-			}
-		}
-	}
-	bfs();
+      if (nx < 0 or nx >= n or ny < 0 or ny >= n) continue;
+      if (isVisited[nx][ny]) continue;
+      if (!isLightOn[nx][ny]) continue;
+
+      isVisited[nx][ny] = true;
+      lightOn({nx, ny});
+      q.push({nx, ny});
+    }
+  }
+
+  while (!lightQ.empty()) {
+    auto cur = lightQ.front();
+    lightQ.pop();
+
+    if (isVisited[cur.first][cur.second]) continue;
+
+    for (int dir = 0; dir < 4; ++dir) {
+      int nx = cur.first + dx[dir];
+      int ny = cur.second + dy[dir];
+      if (nx < 0 or nx >= n or ny < 0 or ny >= n) continue;
+
+      if (isVisited[nx][ny]) {
+        isVisited[cur.first][cur.second] = true;
+        lightOn({cur.first, cur.second});
+        q.push({cur.first, cur.second});
+        break;
+      }
+    }
+  }
+
+  bfs();
 }
+
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  freopen("input.txt", "r", stdin);
 
-	freopen("input.txt", "r", stdin);
+  cin >> n >> m;
+  while (m--) {
+    int a, b, c, d;
+    cin >> a >> b >> c >> d;
+    switchMap[{a - 1, b - 1}].push({c - 1, d - 1});
+  }
 
-	cin >> n >> m;
-	while (m--) {
-		int a, b, c, d;
-		cin >> a >> b >> c >> d;
-		light_m[{a - 1, b - 1}].push({ c - 1,d - 1 });
-	}
+  isVisited[0][0] = true;
+  isLightOn[0][0] = true;
 
-	visit[0][0] = true;
-	light[0][0] = true;
+  lightOn({0, 0});
+  lightQ.push({0, 0});
+  q.push({0, 0});
 
-	light_on({ 0,0 });
-	lq.push({ 0,0 });
-	q.push({ 0,0 });
-	cnt++;
+  bfs();
+  cout << answer;
 
-	bfs();
-
-	//output();
-	cout << cnt;
-
-	return 0;
+  return 0;
 }
