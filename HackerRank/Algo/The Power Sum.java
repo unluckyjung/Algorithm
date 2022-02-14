6b36f92d8c6c7780ddce9157a3137f1ecc8a05c3
unluckyjung <ybook2012@gmail.com>
2022-02-14 23:10:26 +0900

    add(해커랭크 The Power Sum): java풀이
    ## 문제해석
    - X을 어떤수들의 N거듭제곱의 합으로 만들수 있는 경우의 수를 구하라.
    
    ## 해결전략, 구현
    - X가 최대 1000이므로, N은 커봐야 31 까지 가능하다.  (32 * 32 = 1024)
    - DP[숫자] = 숫자를 만드는데 걸리는 경우의 수 
    - DP[숫자] = DP[숫자] + DP[숫자 - 거듭제곱] 라는 점화식을 뽑을 수 있다.
    
    ## 피드백
    - 백준 동전1과 비슷한 유형의 문제이다.
    - 냅색유형의 문제를 좀더 풀어보아야겠다.

---
+++ HackerRank/Algo/The Power Sum.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'powerSum' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER X
+     *  2. INTEGER N
+     */
+
+    
+    public static int powerSum(int X, int N) {
+    // Write your code here
+        // {0 ~ 33}
+        int[] dp = new int[1002];
+        dp[0] = 1;
+        
+        for(int i = 1; i <= 33; ++i){
+            int powNum = (int)Math.pow(i, N);
+            for(int ii = X ; ii >= powNum; --ii){
+                dp[ii] += dp[ii - powNum];
+            }
+        }
+        
+        return dp[X];
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int X = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int result = Result.powerSum(X, N);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
