bc86d6d548c406aac5423e9b04b89e58723fd79e
unluckyjung <ybook2012@gmail.com>
2023-08-20 19:21:09 +0900

    add(백준): 3273 두수의 합
    ## 문제해석
    - 중복되지 않은 자연수의 2개의 합이 원하는 값이 되는지 확인하라.
    
    ## 해결전략
    - 숫자의 등장여부를 기록한다.
    - 필요한숫자 - 입력숫자 가 존재하는지 확인한다.
    - 범위를 넘어가는 숫자를 조회하지 않도록 주의한다.

---
+++ BaekJoon/3273.kt
@@ -0,0 +1,32 @@
+import java.util.*
+
+const val NUM_MAX_SIZE = 1000000
+val numExist = MutableList(NUM_MAX_SIZE + 2) { false }
+val nums = mutableListOf<Int>()
+
+fun main() {
+    val numCount = readln().toInt()
+    val numsString = StringTokenizer(readln())
+
+    repeat(numCount) {
+        val num = numsString.nextToken().toInt()
+        numExist[num] = true
+        nums.add(num)
+    }
+
+    val targetNumber = readln().toInt()
+    var answer = 0
+
+    for (num in nums) {
+        val needNumber = targetNumber - num
+        if (needNumber <= 0 || needNumber > NUM_MAX_SIZE) continue
+        if (numExist[needNumber].not()) continue
+        answer++
+    }
+
+    if (answer == 0) {
+        println(answer)
+    } else {
+        println(answer / 2)
+    }
+}
